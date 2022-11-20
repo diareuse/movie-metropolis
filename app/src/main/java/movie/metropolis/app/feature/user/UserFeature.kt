33c4be7cd949c21f8f3aea5b1623f77ecfe01c4f
Viktor De Pasquale <info@depasquale.wiki>
2022-11-20 12:38:43 +0100

    Updated method name

--- app/src/main/java/movie/metropolis/app/feature/user/UserFeature.kt
+++ app/src/main/java/movie/metropolis/app/feature/user/UserFeature.kt
@@ -3,7 +3,7 @@ package movie.metropolis.app.feature.user
 interface UserFeature {
 
     suspend fun update(data: Iterable<FieldUpdate>): Result<User>
-    suspend fun get(): Result<User>
+    suspend fun getUser(): Result<User>
     suspend fun getBookings(): Result<Iterable<Booking>>
     suspend fun getToken(): Result<String>
 
