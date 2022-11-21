52a418d945a5bdcdef0be522d6af72a858ee1440
Viktor De Pasquale <info@depasquale.wiki>
2022-11-21 17:41:51 +0100

    Added login tests

--- app/src/main/java/movie/metropolis/app/screen/profile/LoginViewModel.kt
+++ app/src/main/java/movie/metropolis/app/screen/profile/LoginViewModel.kt
@@ -2,13 +2,20 @@ package movie.metropolis.app.screen.profile
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import movie.metropolis.app.screen.Loadable
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor() : ViewModel() {
 
+    val state: Flow<Loadable<Boolean>> = TODO()
     val username = MutableStateFlow("")
     val password = MutableStateFlow("")
+
+    fun send() {
+        TODO()
+    }
 
 }
