dfb3eff6a2ee9eb5cadec4ea09eec59d931879da
Viktor De Pasquale <info@depasquale.wiki>
2022-11-21 17:06:09 +0100

    Added listing tests

--- app/src/test/java/movie/metropolis/app/screen/listing/ListingViewModelTest.kt
+++ app/src/test/java/movie/metropolis/app/screen/listing/ListingViewModelTest.kt
@@ -1,6 +1,15 @@
 package movie.metropolis.app.screen.listing
 
+import kotlinx.coroutines.flow.dropWhile
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import movie.metropolis.app.model.MovieView
+import movie.metropolis.app.screen.Loadable
+import movie.metropolis.app.screen.UrlResponder
 import movie.metropolis.app.screen.ViewModelTest
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 class ListingViewModelTest : ViewModelTest() {
 
@@ -8,6 +17,50 @@ class ListingViewModelTest : ViewModelTest() {
 
     override fun prepare() {
         viewModel = ListingViewModel()
+    }
+
+    @Test
+    fun current_returnsList() = runTest {
+        responder.onUrlRespond(
+            UrlResponder.MoviesByShowing("SHOWING"),
+            "data-api-service-films-by-showing-type-SHOWING.json"
+        )
+        val loadable = viewModel.current
+            .dropWhile { it is Loadable.Loading }
+            .first()
+        assertIs<Loadable.Loaded<List<MovieView>>>(loadable)
+        val result = loadable.result
+        assertEquals(34, result.size, "Expected 34 elements, but was: $result")
+    }
+
+    @Test
+    fun current_failsGracefully() = runTest {
+        val loadable = viewModel.current
+            .dropWhile { it is Loadable.Loading }
+            .first()
+        assertIs<Loadable.Error<*>>(loadable)
+    }
+
+    @Test
+    fun upcoming_returnsList() = runTest {
+        responder.onUrlRespond(
+            UrlResponder.MoviesByShowing("FUTURE"),
+            "data-api-service-films-by-showing-type-FUTURE.json"
+        )
+        val loadable = viewModel.upcoming
+            .dropWhile { it is Loadable.Loading }
+            .first()
+        assertIs<Loadable.Loaded<List<MovieView>>>(loadable)
+        val result = loadable.result
+        assertEquals(18, result.size, "Expected 18 elements, but was: $result")
+    }
+
+    @Test
+    fun upcoming_failsGracefully() = runTest {
+        val loadable = viewModel.upcoming
+            .dropWhile { it is Loadable.Loading }
+            .first()
+        assertIs<Loadable.Error<*>>(loadable)
     }
 
 }
