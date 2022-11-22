612c686aeceb3b6fdfd13fa5e21c0af9d85af061
Viktor De Pasquale <info@depasquale.wiki>
2022-11-22 14:12:40 +0100

    Added dependency injection for tests

--- app/src/main/java/movie/metropolis/app/feature/user/di/UserFeatureModule.kt
+++ app/src/main/java/movie/metropolis/app/feature/user/di/UserFeatureModule.kt
@@ -1,19 +1,66 @@
 package movie.metropolis.app.feature.user.di
 
+import android.accounts.AccountManager
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.ktor.client.HttpClient
+import movie.metropolis.app.di.ClientCustomer
+import movie.metropolis.app.feature.user.UserAccount
+import movie.metropolis.app.feature.user.UserAccountImpl
+import movie.metropolis.app.feature.user.UserCredentials
+import movie.metropolis.app.feature.user.UserCredentialsImpl
 import movie.metropolis.app.feature.user.UserFeature
+import movie.metropolis.app.feature.user.UserFeatureImpl
+import movie.metropolis.app.feature.user.UserService
+import movie.metropolis.app.feature.user.UserServiceImpl
+import movie.metropolis.app.feature.user.UserServiceReauthorize
+import movie.metropolis.app.feature.user.UserServiceSaving
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
-class UserFeatureModule {
+internal class UserFeatureModule {
 
     @Provides
-    fun feature(client: HttpClient): UserFeature {
-        TODO()
+    fun feature(
+        service: UserService,
+        account: UserAccount
+    ): UserFeature {
+        return UserFeatureImpl(service, account)
+    }
+
+    @Provides
+    fun service(
+        @ClientCustomer
+        client: HttpClient,
+        account: UserAccount,
+        credentials: UserCredentials
+    ): UserService {
+        var service: UserService
+        service = UserServiceImpl(client, account)
+        service = UserServiceSaving(service, credentials, account)
+        service = UserServiceReauthorize(service, credentials, account)
+        return service
+    }
+
+    @Provides
+    fun account(
+        @ApplicationContext
+        context: Context,
+        credentials: UserCredentials
+    ): UserAccount {
+        return UserAccountImpl(AccountManager.get(context), credentials)
+    }
+
+    @Provides
+    fun credentials(
+        @ApplicationContext
+        context: Context
+    ): UserCredentials {
+        return UserCredentialsImpl(context)
     }
 
 }
