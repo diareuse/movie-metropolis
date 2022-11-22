d4e8b1e8d80b5cb2f8b9dd1535f3b25d25f3e5cb
Viktor De Pasquale <info@depasquale.wiki>
2022-11-22 13:51:29 +0100

    Added user feature implementation

---
+++ app/src/main/java/movie/metropolis/app/feature/user/UserFeatureImpl.kt
@@ -0,0 +1,114 @@
+package movie.metropolis.app.feature.user
+
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import movie.metropolis.app.feature.user.FieldUpdate.*
+import movie.metropolis.app.feature.user.model.BookingResponse
+import movie.metropolis.app.feature.user.model.CustomerDataRequest
+import movie.metropolis.app.feature.user.model.CustomerPointsResponse
+import movie.metropolis.app.feature.user.model.CustomerResponse
+import movie.metropolis.app.feature.user.model.PasswordRequest
+import kotlin.time.Duration.Companion.minutes
+
+internal class UserFeatureImpl(
+    private val service: UserService,
+    private val account: UserAccount
+) : UserFeature {
+
+    override suspend fun update(data: Iterable<FieldUpdate>) = getUser(
+        user = {
+            service.getUser()
+                .map { CustomerDataRequest(it) }
+                .map { data.fold(it, ::updateRequest) }
+                .mapCatching { service.updateUser(it).getOrThrow().customer }
+        },
+        password = {
+            val passwordRequest = data.asSequence()
+                .filterIsInstance<Password>()
+                .map { PasswordRequest(it.old, it.new) }
+                .firstOrNull()
+            if (passwordRequest != null) service.updatePassword(passwordRequest)
+            else Result.success(Unit)
+        }
+    )
+
+    override suspend fun getUser(): Result<User> {
+        return getUser(user = { service.getUser() })
+    }
+
+    override suspend fun getBookings(): Result<Iterable<Booking>> {
+        return service.getBookings().map { it.map(::Booking) }
+    }
+
+    override suspend fun getToken(): Result<String> {
+        if (account.isExpired || account.expiresWithin(1.minutes)) {
+            service.getPoints() // this just refreshes the token
+        }
+        return kotlin.runCatching { checkNotNull(account.token) }
+    }
+
+    // ---
+
+    private fun Booking(item: BookingResponse) = Booking(
+        id = item.id,
+        name = item.name,
+        startsAt = item.startsAt,
+        paidAt = item.paidAt,
+        distributorCode = item.eventMasterCode,
+        eventId = item.eventId
+    )
+
+    private suspend fun getUser(
+        user: suspend () -> Result<CustomerResponse.Customer>,
+        password: suspend () -> Result<Unit> = { Result.success(Unit) },
+    ): Result<User> = coroutineScope {
+        val userResponse = async { user() }
+        val points = async { service.getPoints() }
+        password().mapCatching { asUser(userResponse, points) }
+    }
+
+    @Throws(Throwable::class)
+    private suspend fun asUser(
+        user: Deferred<Result<CustomerResponse.Customer>>,
+        points: Deferred<Result<CustomerPointsResponse>>
+    ): User {
+        val customer = user.await().getOrThrow()
+        val pointsTotal = points.await().getOrThrow().total
+        val consent = User.Consent(
+            marketing = customer.consent.marketing,
+            premium = customer.consent.marketingPremium ?: false
+        )
+        val membership = customer.membership.club?.let { club ->
+            User.Membership(
+                cardNumber = club.cardNumber,
+                memberFrom = club.joinedAt,
+                memberUntil = club.expiredAt
+            )
+        }
+        return User(
+            firstName = customer.firstName,
+            lastName = customer.lastName,
+            email = customer.email,
+            phone = customer.phone,
+            birthAt = customer.birthAt,
+            favoriteCinemaId = customer.favoriteCinema,
+            consent = consent,
+            membership = membership,
+            points = pointsTotal
+        )
+    }
+
+    // ---
+
+    private fun updateRequest(model: CustomerDataRequest, field: FieldUpdate) = when (field) {
+        is Cinema -> model.copy(favoriteCinema = field.id)
+        is Consent.Marketing -> model.copy(consent = model.consent.copy(marketing = field.isEnabled))
+        is Consent.Premium -> model.copy(consent = model.consent.copy(marketingPremium = field.isEnabled))
+        is Email -> model.copy(email = field.value)
+        is Name.First -> model.copy(firstName = field.value)
+        is Name.Last -> model.copy(lastName = field.value)
+        is Phone -> model.copy(phone = field.value)
+        else -> model
+    }
+}
--- app/src/main/java/movie/metropolis/app/feature/user/model/CustomerDataRequest.kt
+++ app/src/main/java/movie/metropolis/app/feature/user/model/CustomerDataRequest.kt
@@ -9,11 +9,24 @@ import java.util.Locale
 internal data class CustomerDataRequest(
     @SerialName("consents") val consent: ConsentRemote,
     @SerialName("email") val email: String,
-    @SerialName("favouriteCinemaId") val favoriteCinema: String,
+    @SerialName("favouriteCinemaId") val favoriteCinema: String?,
     @SerialName("firstName") val firstName: String,
     @SerialName("lastName") val lastName: String,
     @SerialName("phoneNumber") val phone: String,
     @Serializable(LocaleSerializer::class)
     @SerialName("locale") val locale: Locale = Locale.getDefault(),
     @SerialName("type") val type: String = "basic"
-)
+) {
+
+    constructor(
+        customer: CustomerResponse.Customer
+    ) : this(
+        customer.consent,
+        customer.email,
+        customer.favoriteCinema,
+        customer.firstName,
+        customer.lastName,
+        customer.phone
+    )
+
+}
