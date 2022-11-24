945b0d9be0f949c504c965cd30d90db37c4caf81
Viktor De Pasquale <info@depasquale.wiki>
2022-11-24 18:49:41 +0100

    Added home screen ui

---
+++ app/src/main/java/movie/metropolis/app/screen/listing/MovieItem.kt
@@ -0,0 +1,208 @@
+package movie.metropolis.app.screen.listing
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.placeholder
+import com.google.accompanist.placeholder.shimmer
+import movie.metropolis.app.R
+import movie.metropolis.app.model.ImageView
+import movie.metropolis.app.model.VideoView
+import movie.metropolis.app.theme.Theme
+
+@Composable
+fun MovieItem(
+    name: String,
+    subtext: String,
+    video: VideoView?,
+    poster: ImageView?,
+    onClickVideo: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    MovieItemLayout(
+        modifier = modifier,
+        posterAspectRatio = poster?.aspectRatio ?: (4f / 3),
+        poster = {
+            MoviePoster(url = poster?.url)
+            if (video != null) Image(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clickable(role = Role.Button) { onClickVideo(video.url) },
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+        },
+        text = {
+            MovieSubText(text = subtext)
+            MovieTitleText(text = name)
+        }
+    )
+}
+
+@Composable
+fun MovieItem() {
+    MovieItemLayout(
+        poster = { MoviePoster(url = null, modifier = Modifier.fillMaxSize()) },
+        text = {
+            MovieSubText(text = "2021", isLoading = true)
+            Spacer(Modifier.size(4.dp))
+            MovieTitleText(text = "Hi you :)", isLoading = true)
+        }
+    )
+}
+
+@Composable
+fun MovieItemLayout(
+    poster: @Composable BoxScope.() -> Unit,
+    text: @Composable ColumnScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    posterAspectRatio: Float = 3f / 5,
+) {
+    Column(modifier = modifier.width(IntrinsicSize.Min)) {
+        Row(
+            modifier = Modifier.height(225.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .clip(MaterialTheme.shapes.medium)
+                    .fillMaxHeight()
+                    .aspectRatio(posterAspectRatio)
+                    .background(MaterialTheme.colorScheme.surfaceVariant),
+                contentAlignment = Alignment.Center,
+                content = poster
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 12.dp, end = 12.dp)
+                .fillMaxWidth(),
+            content = text
+        )
+    }
+}
+
+@Composable
+fun MoviePoster(url: String?, modifier: Modifier = Modifier) {
+    AsyncImage(
+        modifier = modifier
+            .fillMaxSize()
+            .placeholder(
+                visible = url == null,
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = MaterialTheme.shapes.small,
+                highlight = PlaceholderHighlight.shimmer(MaterialTheme.colorScheme.background)
+            ),
+        model = url ?: "",
+        contentDescription = "",
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun MovieSubText(
+    text: String,
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false
+) {
+    Text(
+        modifier = modifier.placeholder(
+            visible = isLoading,
+            color = MaterialTheme.colorScheme.surfaceVariant,
+            shape = MaterialTheme.shapes.small,
+            highlight = PlaceholderHighlight.shimmer(MaterialTheme.colorScheme.background)
+        ),
+        text = text,
+        style = MaterialTheme.typography.bodySmall
+    )
+}
+
+@Composable
+fun MovieTitleText(
+    text: String,
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false
+) {
+    Text(
+        modifier = modifier.placeholder(
+            visible = isLoading,
+            color = MaterialTheme.colorScheme.surfaceVariant,
+            shape = MaterialTheme.shapes.small,
+            highlight = PlaceholderHighlight.shimmer(MaterialTheme.colorScheme.background)
+        ),
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        fontWeight = FontWeight.Bold,
+        maxLines = 2,
+        minLines = 2,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    Theme {
+        MovieItem(
+            name = "Black Adam",
+            subtext = "12. 11. 2022",
+            video = VideoView("https://www.youtube.com/watch?v=X0tOpBuYasI"),
+            poster = ImageView(
+                0.6741573f,
+                "https://www.cinemacity.cz/xmedia-cw/repo/feats/posters/5145S2R-lg.jpg"
+            ),
+            onClickVideo = {}
+        )
+    }
+}
+
+private fun VideoView(url: String) = object : VideoView {
+    override val url: String
+        get() = url
+
+}
+
+private fun ImageView(
+    aspectRatio: Float,
+    url: String
+) = object : ImageView {
+    override val aspectRatio: Float
+        get() = aspectRatio
+    override val url: String
+        get() = url
+}
--- app/src/main/java/movie/metropolis/app/screen/listing/MoviesScreen.kt
+++ app/src/main/java/movie/metropolis/app/screen/listing/MoviesScreen.kt
@@ -1,8 +1,135 @@
 package movie.metropolis.app.screen.listing
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import movie.metropolis.app.model.ImageView
+import movie.metropolis.app.model.MovieView
+import movie.metropolis.app.model.VideoView
+import movie.metropolis.app.screen.Loadable
+import movie.metropolis.app.theme.Theme
+import kotlin.random.Random.Default.nextBytes
 
 @Composable
-fun MoviesScreen() {
+fun MoviesScreen(
+    viewModel: ListingViewModel = hiltViewModel()
+) {
+    val current by viewModel.current.collectAsState()
+    val upcoming by viewModel.upcoming.collectAsState()
+    MoviesScreen(
+        current = current,
+        upcoming = upcoming
+    )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MoviesScreen(
+    current: Loadable<List<MovieView>>,
+    upcoming: Loadable<List<MovieView>>,
+    onClickVideo: (String) -> Unit = {}
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = {
+                Text("Movies")
+            })
+        }
+    ) { padding ->
+        LazyColumn(contentPadding = padding) {
+            item {
+                Text(
+                    text = "Upcoming Movies",
+                    modifier = Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+            item {
+                MoviePager(
+                    items = upcoming,
+                    isShowing = false,
+                    onClickVideo = onClickVideo
+                )
+            }
+            item {
+                Text(
+                    text = "Showing right now",
+                    modifier = Modifier.padding(start = 24.dp, end = 24.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+            item {
+                MoviePager(
+                    items = current,
+                    isShowing = true,
+                    onClickVideo = onClickVideo
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview(@PreviewParameter(ListMovieViewProvider::class) movies: List<MovieView>) {
+    Theme {
+        MoviesScreen(
+            Loadable.loading(),
+            Loadable.success(movies)
+        )
+    }
+}
+
+class ListMovieViewProvider : CollectionPreviewParameterProvider<List<MovieView>>(buildList {
+    val provider = MovieViewProvider()
+    this += provider.values.toList()
+})
+
+class MovieViewProvider :
+    CollectionPreviewParameterProvider<MovieView>(List(10) { PreviewMovie() }) {
+    private data class PreviewMovie(
+        override val id: String = String(nextBytes(10)),
+        override val name: String = listOf(
+            "Black Adam",
+            "Black Panther: Wakanda Forever",
+            "Strange World",
+            "The Fabelmans"
+        ).random(),
+        override val releasedAt: String = "2022",
+        override val duration: String = "1h 35m",
+        override val availableFrom: String = "21. 3. 2022",
+        override val directors: List<String> = emptyList(),
+        override val cast: List<String> = emptyList(),
+        override val countryOfOrigin: String = "USA",
+        override val poster: ImageView? = PreviewImage(),
+        override val video: VideoView? = PreviewVideo()
+    ) : MovieView
+
+    private data class PreviewImage(
+        override val aspectRatio: Float = 3f / 5,
+        override val url: String = listOf(
+            "https://www.cinemacity.cz/xmedia-cw/repo/feats/posters/5145S2R-lg.jpg",
+            "https://www.cinemacity.cz/xmedia-cw/repo/feats/posters/5219S2R-lg.jpg",
+            "https://www.cinemacity.cz/xmedia-cw/repo/feats/posters/5228D2R-lg.jpg",
+            "https://www.cinemacity.cz/xmedia-cw/repo/feats/posters/5392S2R-lg.jpg"
+        ).random()
+    ) : ImageView
+
+    private data class PreviewVideo(
+        override val url: String = ""
+    ) : VideoView
 }
