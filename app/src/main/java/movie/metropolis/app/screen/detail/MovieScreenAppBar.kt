fe3befe0e7d438e338b36f13a7491687d10a0ebc
Viktor De Pasquale <info@depasquale.wiki>
2022-11-26 20:02:18 +0100

    Updated movie detail design

--- app/src/main/java/movie/metropolis/app/screen/detail/MovieScreenAppBar.kt
+++ app/src/main/java/movie/metropolis/app/screen/detail/MovieScreenAppBar.kt
@@ -1,38 +1,45 @@
 package movie.metropolis.app.screen.detail
 
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import movie.metropolis.app.R
 import movie.metropolis.app.theme.Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieScreenAppBar(
-    scrollBehavior: TopAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
     onBackClick: () -> Unit
 ) {
-    CenterAlignedTopAppBar(
+    TopAppBar(
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.Transparent),
-        scrollBehavior = scrollBehavior,
         navigationIcon = {
-            IconButton(onClick = onBackClick) {
+            IconButton(
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(MaterialTheme.colorScheme.surface),
+                onClick = onBackClick
+            ) {
                 Icon(painterResource(id = R.drawable.ic_back), null)
             }
         },
-        title = { Text("Movie Detail") }
+        title = {}
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 private fun Preview() {
