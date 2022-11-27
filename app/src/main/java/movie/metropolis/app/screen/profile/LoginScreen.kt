1b04b37157dc81c49ded3dca5ed31d44f250fbf5
Viktor De Pasquale <info@depasquale.wiki>
2022-11-27 16:09:26 +0100

    Added login screen implementation

--- app/src/main/java/movie/metropolis/app/screen/profile/LoginScreen.kt
+++ app/src/main/java/movie/metropolis/app/screen/profile/LoginScreen.kt
@@ -8,12 +8,14 @@ import movie.metropolis.app.model.LoginMode
 @Composable
 fun LoginScreen(
     onNavigateHome: () -> Unit,
+    onBackClick: () -> Unit,
     viewModel: LoginViewModel = hiltViewModel()
 ) {
     when (viewModel.mode.collectAsState().value) {
         LoginMode.Login -> LoginSignInScreen(
-            viewModel,
-            onNavigateHome = onNavigateHome
+            viewModel = viewModel,
+            onNavigateHome = onNavigateHome,
+            onBackClick = onBackClick
         )
 
         LoginMode.Registration -> LoginRegistrationScreen(
