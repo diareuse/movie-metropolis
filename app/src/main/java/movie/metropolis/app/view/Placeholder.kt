3a7cd6638db3291e01409775df17ea254c1bd511
Viktor De Pasquale <info@depasquale.wiki>
2022-11-27 21:25:29 +0100

    Updated placeholder to take color current content

--- app/src/main/java/movie/metropolis/app/view/Placeholder.kt
+++ app/src/main/java/movie/metropolis/app/view/Placeholder.kt
@@ -1,10 +1,13 @@
 package movie.metropolis.app.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.placeholder.PlaceholderHighlight
 import com.google.accompanist.placeholder.placeholder
@@ -30,10 +33,27 @@ fun Modifier.imagePlaceholder(visible: Boolean, shape: ShapeToken = ShapeToken.M
 }
 
 fun Modifier.textPlaceholder(visible: Boolean, shape: ShapeToken = ShapeToken.Small) = composed {
+    val color = LocalContentColor.current
     padding(if (visible) 2.dp else 0.dp).placeholder(
         visible = visible,
-        color = MaterialTheme.colorScheme.surfaceVariant,
+        color = color.copy(alpha = .1f),
         shape = shape.shape,
-        highlight = PlaceholderHighlight.shimmer(MaterialTheme.colorScheme.background)
+        highlight = PlaceholderHighlight.shimmer(MaterialTheme.colorScheme.containerColorFor(color))
     )
+}
+
+private fun ColorScheme.containerColorFor(color: Color) = when (color) {
+    onPrimary -> primary
+    onSecondary -> secondary
+    onTertiary -> tertiary
+    onBackground -> background
+    onError -> error
+    onSurface -> surface
+    onSurfaceVariant -> surfaceVariant
+    onPrimaryContainer -> primaryContainer
+    onSecondaryContainer -> secondaryContainer
+    onTertiaryContainer -> tertiaryContainer
+    onErrorContainer -> errorContainer
+    inverseOnSurface -> inverseSurface
+    else -> Color.Unspecified
 }
