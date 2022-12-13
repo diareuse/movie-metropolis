47a6d22884b578601054d1a3b26c647458940219
Viktor De Pasquale <info@depasquale.wiki>
2022-12-13 19:02:02 +0100

    Removed divider

--- app/src/main/java/movie/metropolis/app/screen/Navigation.kt
+++ app/src/main/java/movie/metropolis/app/screen/Navigation.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -180,7 +178,6 @@ fun HomeScreen(
                         scrolledContainerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.9f)
                     )
                 )
-                Divider(modifier = Modifier.padding(horizontal = 16.dp))
             }
         },
         bottomBar = {
