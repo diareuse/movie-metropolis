4d86139c0cdf2efafc9a0cde824bca66d41c8817
Viktor De Pasquale <info@depasquale.wiki>
2022-12-13 14:20:18 +0100

    Updated movie detail layout

--- app/src/main/java/movie/metropolis/app/screen/detail/MovieScreenAppBar.kt
+++ app/src/main/java/movie/metropolis/app/screen/detail/MovieScreenAppBar.kt
@@ -1,16 +1,15 @@
 package movie.metropolis.app.screen.detail
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,14 +25,18 @@ fun MovieScreenAppBar(
     TopAppBar(
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.Transparent),
         navigationIcon = {
-            IconButton(
-                modifier = Modifier
-                    .padding(start = 12.dp)
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(MaterialTheme.colorScheme.surface),
-                onClick = onBackClick
+            Surface(
+                modifier = Modifier.padding(start = 12.dp),
+                color = MaterialTheme.colorScheme.surface,
+                tonalElevation = 1.dp,
+                shadowElevation = 16.dp,
+                shape = MaterialTheme.shapes.medium
             ) {
-                Icon(painterResource(id = R.drawable.ic_back), null)
+                IconButton(
+                    onClick = onBackClick
+                ) {
+                    Icon(painterResource(id = R.drawable.ic_back), null)
+                }
             }
         },
         title = {}
