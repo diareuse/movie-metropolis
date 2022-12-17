69950050e3e39930a06b8d38e2e5aae3e44e2f2f
Viktor De Pasquale <info@depasquale.wiki>
2022-12-17 14:46:16 +0100

    Added favorite listing facade implementation

--- app/src/main/java/movie/metropolis/app/screen/listing/ListingViewModel.kt
+++ app/src/main/java/movie/metropolis/app/screen/listing/ListingViewModel.kt
@@ -3,6 +3,8 @@ package movie.metropolis.app.screen.listing
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import movie.metropolis.app.model.MovieView
 import movie.metropolis.app.screen.Loadable
 import movie.metropolis.app.screen.listing.ListingFacade.Companion.currentFlow
 import movie.metropolis.app.screen.listing.ListingFacade.Companion.upcomingFlow
@@ -11,12 +13,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ListingViewModel @Inject constructor(
-    facade: ListingFacade
+    private val facade: ListingFacade
 ) : ViewModel() {
 
     val current = facade.currentFlow
         .retainStateIn(viewModelScope, Loadable.loading())
     val upcoming = facade.upcomingFlow
         .retainStateIn(viewModelScope, Loadable.loading())
+
+    fun toggleFavorite(movie: MovieView) = viewModelScope.launch {
+        facade.toggleFavorite(movie)
+    }
 
 }
