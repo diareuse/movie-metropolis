66a016412b6aaaa77635dea2e1168db2ec55a67d
Viktor De Pasquale <info@depasquale.wiki>
2022-12-18 13:04:48 +0100

    Reverted setting immediate notifications

--- feature-core/src/main/java/movie/core/FavoriteFeatureScheduleNotification.kt
+++ feature-core/src/main/java/movie/core/FavoriteFeatureScheduleNotification.kt
@@ -4,8 +4,6 @@ import movie.core.model.MoviePreview
 import movie.core.pulse.ExactPulseNotificationMovie
 import movie.pulse.ExactPulseRequest
 import movie.pulse.ExactPulseScheduler
-import java.util.Date
-import kotlin.time.Duration.Companion.minutes
 
 class FavoriteFeatureScheduleNotification(
     private val origin: FavoriteFeature,
@@ -16,7 +14,7 @@ class FavoriteFeatureScheduleNotification(
         movie: MoviePreview
     ) = origin.toggle(movie).onSuccess { isFavorite ->
         val request = ExactPulseRequest.Builder<ExactPulseNotificationMovie>()
-            .setDate(Date(System.currentTimeMillis() + 1.minutes.inWholeMilliseconds))//(movie.screeningFrom)
+            .setDate(movie.screeningFrom)
             .setData(ExactPulseNotificationMovie.getData(movie))
             .build()
         when (isFavorite) {
