468cff12f99164c8019db30737151cbac25542d3
Viktor De Pasquale <info@depasquale.wiki>
2022-12-19 14:03:17 +0100

    Added csfd rating provider

---
+++ feature-rating/src/test/java/movie/rating/RatingProviderCsfdTest.kt
@@ -0,0 +1,27 @@
+package movie.rating
+
+import io.ktor.client.request.HttpRequestData
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class RatingProviderCsfdTest : AbstractRatingTest() {
+
+    override fun prepare() {}
+
+    override fun respond(request: HttpRequestData): String {
+        if (!request.url.host.contains("csfd"))
+            throw IllegalArgumentException()
+        return when {
+            request.url.toString().contains("/film/") -> "csfd-detail.html"
+            else -> "csfd-query.html"
+        }
+    }
+
+    @Test
+    fun returns_csfd() = runTest {
+        val result = provider.getRating(descriptor)
+        assertEquals(86, result)
+    }
+
+}
