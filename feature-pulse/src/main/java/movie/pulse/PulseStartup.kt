cf7a896ed533746aeca85cd129f0d32c6c4987a7
Viktor De Pasquale <info@depasquale.wiki>
2022-12-20 10:04:02 +0100

    Added refresh request on startup

--- feature-pulse/src/main/java/movie/pulse/PulseStartup.kt
+++ feature-pulse/src/main/java/movie/pulse/PulseStartup.kt
@@ -5,30 +5,37 @@ import androidx.startup.Initializer
 import androidx.work.BackoffPolicy
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.ExistingWorkPolicy
 import androidx.work.NetworkType
-import androidx.work.PeriodicWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
 
 class PulseStartup : Initializer<Unit> {
 
+    private val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .build()
+
     override fun create(context: Context) {
         val manager = WorkManager.getInstance(context)
         val daily = createDailyRequest()
+        val instant = createInstantRequest()
         manager.enqueueUniquePeriodicWork("daily", ExistingPeriodicWorkPolicy.KEEP, daily)
+        manager.enqueueUniqueWork("refresh", ExistingWorkPolicy.KEEP, instant)
     }
 
-    private fun createDailyRequest(): PeriodicWorkRequest {
-        val constraints = Constraints.Builder()
-            .setRequiresStorageNotLow(true)
-            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .build()
-        return PeriodicWorkRequestBuilder<PulseDailyWorker>(1, TimeUnit.DAYS)
+    private fun createDailyRequest() =
+        PeriodicWorkRequestBuilder<PulseDailyWorker>(1, TimeUnit.DAYS)
             .setConstraints(constraints)
             .setBackoffCriteria(BackoffPolicy.LINEAR, 10, TimeUnit.MINUTES)
             .build()
-    }
+
+    private fun createInstantRequest() = OneTimeWorkRequestBuilder<PulseDailyWorker>()
+        .setConstraints(constraints)
+        .setBackoffCriteria(BackoffPolicy.LINEAR, 10, TimeUnit.MINUTES)
+        .build()
 
     override fun dependencies() = listOf(WorkManagerStartup::class.java)
 
