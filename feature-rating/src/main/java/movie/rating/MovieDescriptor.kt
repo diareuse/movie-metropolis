ca2f25cdb4a9475778512b8ba78013eff92f4f65
Viktor De Pasquale <info@depasquale.wiki>
2022-12-20 09:43:15 +0100

    Updated movie descriptor to sanitize the name

--- feature-rating/src/main/java/movie/rating/MovieDescriptor.kt
+++ feature-rating/src/main/java/movie/rating/MovieDescriptor.kt
@@ -1,6 +1,33 @@
 package movie.rating
 
-data class MovieDescriptor(
-    val name: String,
+class MovieDescriptor(
+    name: String,
     val year: Int
-)
+) {
+
+    val name = name
+        .replace(Regex("\\d[Dd][Xx]?(\\s*-\\s*(sub|dub))?"), "")
+        .replace("the", "", true)
+        .replace("  ", " ")
+        .trim()
+
+    operator fun component1() = name
+    operator fun component2() = year
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is MovieDescriptor) return false
+
+        if (name != other.name) return false
+        if (year != other.year) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + year
+        return result
+    }
+
+}
