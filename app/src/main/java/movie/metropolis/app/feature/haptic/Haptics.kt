4ea043de267ec54d8708171d36a6e99db7b324e4
Viktor De Pasquale <info@depasquale.wiki>
2023-01-04 13:12:19 +0100

    Added ticks on scroll

--- app/src/main/java/movie/metropolis/app/feature/haptic/Haptics.kt
+++ app/src/main/java/movie/metropolis/app/feature/haptic/Haptics.kt
@@ -2,15 +2,19 @@ package movie.metropolis.app.feature.haptic
 
 import android.view.HapticFeedbackConstants
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.hapticfeedback.HapticFeedback
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 
-
 val Haptics
     @Composable get() = LocalHapticFeedback.current
 
-fun HapticFeedback.click(listener: () -> Any): () -> Unit = {
+fun <T> HapticFeedback.click(listener: () -> T): () -> Unit = {
     performHapticFeedback(HapticFeedbackType(HapticFeedbackConstants.CONTEXT_CLICK))
     listener()
 }
@@ -19,7 +23,19 @@ fun HapticFeedback.tick() =
     performHapticFeedback(HapticFeedbackType(HapticFeedbackConstants.CLOCK_TICK))
 
 @Composable
-fun hapticClick(listener: () -> Any) = Haptics.click(listener)
+fun <T> hapticClick(listener: () -> T) = Haptics.click(listener)
 
 @Composable
 fun (() -> Any).withHaptics() = hapticClick(this)
+
+@Composable
+fun <T> TickOnChange(key: T) {
+    var last by rememberSaveable { mutableStateOf(key) }
+    val haptics = Haptics
+    LaunchedEffect(key) {
+        if (last != key) {
+            haptics.tick()
+            last = key
+        }
+    }
+}
