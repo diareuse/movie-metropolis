4b234fbd8078f7ad74ee5765f1ca64cf033edd85
Viktor De Pasquale <info@depasquale.wiki>
2023-01-04 09:23:00 +0100

    Added missing home facade tests

---
+++ app/src/test/java/movie/metropolis/app/screen/home/HomeFacadeTest.kt
@@ -0,0 +1,30 @@
+package movie.metropolis.app.screen.home
+
+import movie.metropolis.app.di.FacadeModule
+import movie.metropolis.app.screen.FeatureTest
+import org.junit.Test
+import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class HomeFacadeTest : FeatureTest() {
+
+    private lateinit var facade: HomeFacade
+
+    override fun prepare() {
+        facade = FacadeModule().home(user)
+    }
+
+    @Test
+    fun email_returns() {
+        val email = "foo@foo.com"
+        whenever(user.email).thenReturn(email)
+        assertEquals(email, facade.email)
+    }
+
+    @Test
+    fun email_returns_null() {
+        assertNull(facade.email)
+    }
+
+}
