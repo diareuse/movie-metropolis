30aa10fca770c382ca76658757b79eb4cc9e4206
Viktor De Pasquale <info@depasquale.wiki>
2023-01-11 17:02:59 +0100

    Added pulse to reschedule alarms

---
+++ feature-core/src/main/java/movie/core/pulse/PulseScheduling.kt
@@ -0,0 +1,19 @@
+package movie.core.pulse
+
+import android.content.Context
+import android.content.Intent
+import movie.core.startup.BootCompletedWorker
+import movie.pulse.Pulse
+
+class PulseScheduling(
+    private val context: Context
+) : Pulse {
+
+    override suspend fun execute(): Result<Unit> {
+        val intent = Intent(context, BootCompletedWorker::class.java)
+            .setAction(Intent.ACTION_BOOT_COMPLETED)
+        context.sendBroadcast(intent)
+        return Result.success(Unit)
+    }
+
+}
