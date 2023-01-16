9c996f06b0542774871c0d3e67d7fc49eaeb50c6
Viktor De Pasquale <info@depasquale.wiki>
2023-01-16 12:42:46 +0100

    Added error states for booking items

---
+++ style/src/main/java/movie/style/AppErrorItem.kt
@@ -0,0 +1,36 @@
+package movie.style
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import movie.style.theme.Theme
+
+@Composable
+fun AppErrorItem(
+    modifier: Modifier = Modifier,
+    error: String
+) {
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text("🥺", style = Theme.textStyle.title.copy(fontSize = 48.sp))
+        Text(error)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    Theme {
+        AppErrorItem(error = "Something went wrong")
+    }
+}
