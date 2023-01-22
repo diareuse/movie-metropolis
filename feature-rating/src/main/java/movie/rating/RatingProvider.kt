369d345726377c6f619c7a401440bf163ffdbe72
Viktor De Pasquale <info@depasquale.wiki>
2023-01-22 15:34:14 +0100

    Updated rating provider to be heavily parallelized

--- feature-rating/src/main/java/movie/rating/RatingProvider.kt
+++ feature-rating/src/main/java/movie/rating/RatingProvider.kt
@@ -1,11 +1,18 @@
 package movie.rating
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
 interface RatingProvider {
 
     suspend fun getRating(descriptor: MovieDescriptor): Byte
 
 }
 
-suspend fun RatingProvider.getRating(vararg descriptors: MovieDescriptor) = descriptors
-    .distinct()
-    .firstNotNullOfOrNull { getRating(it).takeUnless { it == 0.toByte() } } ?: 0
+suspend fun RatingProvider.getRating(vararg descriptors: MovieDescriptor) = coroutineScope {
+    descriptors
+        .map { async { getRating(it) } }
+        .awaitAll()
+        .firstNotNullOfOrNull { rating -> rating.takeUnless { it == 0.toByte() } } ?: 0
+}
