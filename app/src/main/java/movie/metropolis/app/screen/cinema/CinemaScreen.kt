ae49a02440b58fd689cb3fb1dbeea276b2b3641f
Viktor De Pasquale <info@depasquale.wiki>
2023-01-31 12:46:27 +0100

    Fixed cinema layout

--- app/src/main/java/movie/metropolis/app/screen/cinema/CinemaScreen.kt
+++ app/src/main/java/movie/metropolis/app/screen/cinema/CinemaScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import movie.metropolis.app.R
@@ -89,11 +87,9 @@ private fun CinemaScreen(
                 title = {
                     Text(
                         modifier = Modifier
-                            .fillMaxWidth(.8f)
                             .textPlaceholder(cinema.isLoading),
                         text = cinema.getOrNull()?.name?.substringBefore(',')
-                            ?: stringResource(R.string.loading),
-                        textAlign = TextAlign.Center
+                            ?: stringResource(R.string.loading)
                     )
                 },
                 navigationIcon = {
