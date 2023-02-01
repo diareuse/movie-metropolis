fd1e331d9b38aa062cf5f91f9d31ff38a7662e17
Viktor De Pasquale <info@depasquale.wiki>
2023-02-01 13:21:24 +0100

    Added shortcuts
    - first is for tickets and is permanent
    - second is changing based on the last visited (in-app) location

---
+++ app/src/main/java/movie/metropolis/app/feature/shortcut/ShortcutFactory.kt
@@ -0,0 +1,103 @@
+package movie.metropolis.app.feature.shortcut
+
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import androidx.core.net.toUri
+import movie.metropolis.app.MainActivity
+
+interface ShortcutFactory {
+
+    fun setId(id: String): ShortcutFactory
+    fun setLabel(label: String): ShortcutFactory
+    fun setIcon(icon: Int): ShortcutFactory
+    fun setRoute(route: String): ShortcutFactory
+    fun create()
+
+    companion object {
+
+        fun default(context: Context): ShortcutFactory {
+            return ShortcutFactoryDefault(context)
+        }
+
+        fun unique(context: Context): ShortcutFactory {
+            return ShortcutFactoryUnique(ShortcutFactoryDefault(context))
+        }
+
+    }
+
+}
+
+private class ShortcutFactoryUnique(
+    private val origin: ShortcutFactoryDefault
+) : ShortcutFactory {
+
+    override fun setId(id: String) = apply {
+        origin.setId(id)
+    }
+
+    override fun setLabel(label: String) = apply {
+        origin.setLabel(label)
+    }
+
+    override fun setIcon(icon: Int) = apply {
+        origin.setIcon(icon)
+    }
+
+    override fun setRoute(route: String) = apply {
+        origin.setRoute(route)
+    }
+
+    override fun create() {
+        val shortcuts = ShortcutManagerCompat.getDynamicShortcuts(origin.context)
+        val removable = shortcuts.asSequence()
+            .filter { it.id == origin.id }
+            .map { it.id }
+            .toList()
+        ShortcutManagerCompat.removeDynamicShortcuts(origin.context, removable)
+        origin.create()
+    }
+
+}
+
+private class ShortcutFactoryDefault(
+    val context: Context
+) : ShortcutFactory {
+
+    private lateinit var intent: Intent
+    private lateinit var icon: IconCompat
+    private lateinit var label: String
+    lateinit var id: String
+
+    override fun setId(id: String) = apply {
+        this.id = id
+    }
+
+    override fun setLabel(label: String) = apply {
+        this.label = label
+    }
+
+    override fun setIcon(icon: Int) = apply {
+        this.icon = IconCompat.createWithResource(context, icon)
+    }
+
+    override fun setRoute(route: String) = apply {
+        this.intent = Intent(context, MainActivity::class.java)
+            .setAction(Intent.ACTION_VIEW)
+            .setData("app://movie.metropolis/$route".toUri())
+            .setPackage(context.packageName)
+    }
+
+    override fun create() {
+        val shortcut = ShortcutInfoCompat.Builder(context, id)
+            .setShortLabel(label)
+            .setLongLabel(label)
+            .setIcon(icon)
+            .setIntent(intent)
+            .build()
+        ShortcutManagerCompat.pushDynamicShortcut(context, shortcut)
+    }
+
+}
