0d801eaadb43f8ad533c45c658273e6d68bb11a3
Viktor De Pasquale <info@depasquale.wiki>
2023-02-02 16:01:55 +0100

    Updated ratings to by dynamically updated as then become available

--- feature-core/src/main/java/movie/core/EventPreviewFeatureNetworkRating.kt
+++ feature-core/src/main/java/movie/core/EventPreviewFeatureNetworkRating.kt
@@ -1,5 +1,7 @@
 package movie.core
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import movie.core.EventDetailFeature.Companion.get
 import movie.core.adapter.MoviePreviewWithRating
 import movie.core.model.MoviePreview
@@ -9,20 +11,22 @@ class EventPreviewFeatureNetworkRating(
     private val detail: EventDetailFeature
 ) : EventPreviewFeature {
 
-    override suspend fun get(result: ResultCallback<List<MoviePreview>>) {
+    override suspend fun get(result: ResultCallback<List<MoviePreview>>) = coroutineScope {
         var last: Result<List<MoviePreview>> = Result.failure(IllegalStateException())
         origin.get {
             result(it)
             last = it
         }
-        val output = last.map { movies ->
-            movies.map { movie ->
-                val rating = movie.rating
-                if (rating != null && rating > 0) movie
-                else MoviePreviewWithRating(movie, getRating(movie))
+        val movies = last.getOrNull() ?: return@coroutineScope
+        val updated = movies.toMutableList()
+        for ((index, movie) in movies.withIndex()) {
+            val rating = movie.rating
+            if (rating != null && rating > 0) continue
+            launch {
+                updated[index] = MoviePreviewWithRating(movie, getRating(movie))
+                result(Result.success(updated))
             }
         }
-        result(output)
     }
 
     private suspend fun getRating(movie: MoviePreview): Byte? {
