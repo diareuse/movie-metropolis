67283df73ab0abe633c53ed2bbdaa437081d5431
Viktor De Pasquale <info@depasquale.wiki>
2023-02-13 12:53:40 +0100

    Added partial displaying showings for specific movie

--- feature-core/src/main/java/movie/core/EventShowingsFeatureMovieNetwork.kt
+++ feature-core/src/main/java/movie/core/EventShowingsFeatureMovieNetwork.kt
@@ -1,8 +1,12 @@
 package movie.core
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import movie.core.adapter.ShowingFromResponse
+import movie.core.model.Cinema
 import movie.core.model.Location
 import movie.core.model.Movie
+import movie.core.model.Showing
 import movie.core.nwk.EventService
 import java.util.Date
 
@@ -13,17 +17,20 @@ class EventShowingsFeatureMovieNetwork(
     private val cinema: EventCinemaFeature
 ) : EventShowingsFeature.Movie {
 
-    override suspend fun get(date: Date, result: ResultCallback<CinemaWithShowings>) {
+    override suspend fun get(
+        date: Date,
+        result: ResultCallback<CinemaWithShowings>
+    ) = coroutineScope {
         cinema.get(location) { cinemas ->
-            val output = cinemas.getOrThrow().associateWith { cinema ->
-                service.getEventsInCinema(cinema.id, date)
+            val output = mutableMapOf<Cinema, Iterable<Showing>>()
+            for (cinema in cinemas.getOrThrow()) launch {
+                output[cinema] = service.getEventsInCinema(cinema.id, date)
                     .getOrThrow().body.events
                     .filter { it.movieId == movie.id }
                     .map { ShowingFromResponse(it, cinema) }
+                result(Result.success(output))
             }
-            result(Result.success(output))
         }
     }
 
 }
-
