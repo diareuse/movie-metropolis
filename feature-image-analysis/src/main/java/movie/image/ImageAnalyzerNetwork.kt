db3b7395c9721b3c401c394310372a43a6a8a022
Viktor De Pasquale <info@depasquale.wiki>
2023-02-14 13:25:20 +0100

    Updated image analysis to gather info from smaller images

--- feature-image-analysis/src/main/java/movie/image/ImageAnalyzerNetwork.kt
+++ feature-image-analysis/src/main/java/movie/image/ImageAnalyzerNetwork.kt
@@ -12,15 +12,10 @@ import java.net.URL
 internal class ImageAnalyzerNetwork : ImageAnalyzer {
 
     override suspend fun getColors(url: String): Swatch {
-        val bitmap = withContext(Dispatchers.IO) {
-            URL(url).openStream().use { stream ->
-                BitmapFactory.decodeStream(stream, null, getOptions())
-            }
-        }
+        val bitmap = getImage(URL(url))
         requireNotNull(bitmap)
         val palette = withContext(Dispatchers.Default) {
             Palette.from(bitmap)
-                .resizeBitmapArea(200)
                 .addTarget(Target.VIBRANT)
                 .addTarget(Target.LIGHT_MUTED)
                 .addTarget(Target.DARK_MUTED)
@@ -34,10 +29,50 @@ internal class ImageAnalyzerNetwork : ImageAnalyzer {
         )
     }
 
-    private fun Palette.Swatch.asSwatch(): SwatchColor = SwatchColor(rgb or 0xff000000.toInt())
+    private fun Palette.Swatch.asSwatch(): SwatchColor =
+        SwatchColor(rgb or 0xff000000.toInt())
+
+    private suspend fun getImage(url: URL): Bitmap? {
+        val options = getOptions()
+        decodeBitmap(url, options)
+        options.calculateInSampleSize(100, 100)
+        return decodeBitmap(url, options)
+    }
+
+    private suspend fun decodeBitmap(
+        url: URL,
+        options: BitmapFactory.Options
+    ) = withContext(Dispatchers.IO) {
+        url.openStream().use {
+            BitmapFactory.decodeStream(it, null, options)
+        }
+    }
+
+    private fun BitmapFactory.Options.calculateInSampleSize(reqWidth: Int, reqHeight: Int) {
+        val height = outHeight
+        val width = outWidth
+        inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight = height / 2
+            val halfWidth = width / 2
+
+            while (
+                halfHeight / inSampleSize >= reqHeight &&
+                halfWidth / inSampleSize >= reqWidth
+            ) {
+                inSampleSize *= 2
+            }
+        }
+
+        inJustDecodeBounds = false
+    }
+
 
     private fun getOptions() = BitmapFactory.Options().apply {
         inPreferredConfig = Bitmap.Config.RGB_565
+        inJustDecodeBounds = true
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             outConfig = Bitmap.Config.RGB_565
         }
