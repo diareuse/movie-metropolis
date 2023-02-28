cf90a40fb990a34ef8762656b38b739596144251
Viktor De Pasquale <info@depasquale.wiki>
2023-02-28 11:46:22 +0100

    Removed deprecated pieces of ui

--- app/src/main/java/movie/metropolis/app/screen/booking/component/Table.kt
+++ app/src/main/java/movie/metropolis/app/screen/booking/component/Table.kt
@@ -1,43 +1,8 @@
 package movie.metropolis.app.screen.booking.component
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.text.*
-import androidx.compose.ui.text.style.*
-import movie.style.state.ImmutableList
-import movie.style.state.ImmutableList.Companion.immutable
-
-@Composable
-fun Table(
-    vararg rows: TableRow,
-    modifier: Modifier = Modifier,
-    textAlign: TextAlign? = null
-) = Table(
-    rows = rows.toList().immutable(),
-    modifier = modifier,
-    textAlign = textAlign
-)
-
-@Composable
-fun Table(
-    rows: ImmutableList<TableRow>,
-    modifier: Modifier = Modifier,
-    textAlign: TextAlign? = null
-) {
-    Column(modifier = modifier) {
-        for (row in rows) Row {
-            for (column in row.columns)
-                Text(
-                    text = column,
-                    modifier = Modifier.weight(1f),
-                    style = row.textStyle ?: LocalTextStyle.current,
-                    textAlign = textAlign
-                )
-        }
-    }
-}
 
 @Composable
 fun Table(
@@ -57,14 +22,4 @@ fun Table(
             }
         }
     }
-}
-
-@Stable
-data class TableRow(
-    val columns: ImmutableList<String>,
-    val textStyle: TextStyle?
-) {
-    constructor(vararg columns: String, style: TextStyle? = null) : this(
-        columns.toList().immutable(), style
-    )
 }
