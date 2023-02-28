b73a5c8ae4f16729151f3a62ca03a986703d1337
Viktor De Pasquale <info@depasquale.wiki>
2023-02-28 10:48:51 +0100

    Fixed table alignment

--- app/src/main/java/movie/metropolis/app/screen/booking/component/Table.kt
+++ app/src/main/java/movie/metropolis/app/screen/booking/component/Table.kt
@@ -44,12 +44,14 @@ fun Table(
     rows: Int,
     columns: Int,
     modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.Center,
     content: @Composable (row: Int, column: Int) -> Unit
 ) {
     Column(modifier = modifier) {
         for (row in 0 until rows) Row {
             for (column in 0 until columns) Box(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f),
+                contentAlignment = contentAlignment
             ) {
                 content(row, column)
             }
