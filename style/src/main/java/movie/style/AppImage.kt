6f4cc25375d9c43015d234abdaa6ec2f81b7e7fd
Viktor De Pasquale <info@depasquale.wiki>
2023-03-01 09:08:04 +0100

    Updated app image layout

--- style/src/main/java/movie/style/AppImage.kt
+++ style/src/main/java/movie/style/AppImage.kt
@@ -7,16 +7,31 @@ import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.painter.*
 import androidx.compose.ui.layout.*
+import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.*
 import coil.compose.AsyncImage
 import movie.style.image.rememberImageRequest
-import movie.style.theme.Theme
 
 @Composable
 fun AppImage(
     url: String?,
     modifier: Modifier = Modifier,
     placeholder: Painter? = null,
+    alignment: Alignment = Alignment.Center
+) = AppImage(
+    modifier = modifier.imagePlaceholder(url == null),
+    url = url.orEmpty(),
+    placeholder = placeholder,
+    placeholderError = painterResource(id = R.drawable.ic_image_error),
+    alignment = alignment
+)
+
+@Composable
+fun AppImage(
+    url: String,
+    modifier: Modifier = Modifier,
+    placeholder: Painter? = null,
+    placeholderError: Painter? = null,
     alignment: Alignment = Alignment.Center
 ) {
     var size by remember { mutableStateOf(IntSize.Zero) }
@@ -27,13 +42,13 @@ fun AppImage(
         else ColorFilter.tint(LocalContentColor.current)
     AsyncImage(
         modifier = modifier
-            .imagePlaceholder(isLoading || url == null, Theme.container.button)
+            .imagePlaceholder(isLoading)
             .onGloballyPositioned { size = it.size },
         model = rememberImageRequest(url, size),
         contentDescription = null,
         placeholder = placeholder,
-        error = placeholder,
-        fallback = placeholder,
+        error = placeholderError,
+        fallback = placeholderError,
         colorFilter = colorFilter,
         contentScale = ContentScale.Crop,
         alignment = alignment,
