ee2da83a6de353ce8b5fcc12b5fc6e0d3222e012
Viktor De Pasquale <info@depasquale.wiki>
2023-03-02 08:01:37 +0100

    Updated barcode viewer to show progress bars

--- style/src/main/java/movie/style/Barcode.kt
+++ style/src/main/java/movie/style/Barcode.kt
@@ -2,6 +2,8 @@ package movie.style
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
@@ -18,27 +20,30 @@ import movie.style.util.findActivity
 @Composable
 fun Barcode(
     code: String,
-    format: BarcodeFormat,
     modifier: Modifier = Modifier,
+    format: BarcodeFormat = BarcodeFormat.QR_CODE,
     color: Color = Theme.color.container.background
 ) {
     val (size, onSizeChanged) = remember { mutableStateOf(IntSize.Zero) }
-    var image by remember { mutableStateOf(null as ImageBitmap?) }
-    LaunchedEffect(size, code, format) {
-        val writer = MultiFormatWriter()
-        image = withContext(Dispatchers.Default) {
-            val w = size.width
-            val h = size.height
-            if (w * h <= 0) return@withContext null
-            val matrix = writer.encode(code, format, w, h)
-            val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-            for (x in 0 until matrix.width) for (y in 0 until matrix.height) {
-                val pixel = if (matrix.get(x, y)) color.toArgb() else Color.Transparent.toArgb()
-                bitmap.setPixel(x, y, pixel)
-            }
-            bitmap.asImageBitmap()
-        }
+    val image by rememberBarcodeAsState(color = color, size = size, code = code, format = format)
+    val bitmap = image
+    if (bitmap != null) BarcodeViewer(
+        modifier = modifier.onSizeChanged(onSizeChanged),
+        bitmap = bitmap
+    )
+    else Box(
+        modifier = modifier.onSizeChanged(onSizeChanged),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(strokeCap = StrokeCap.Round)
     }
+}
+
+@Composable
+fun BarcodeViewer(
+    bitmap: ImageBitmap,
+    modifier: Modifier = Modifier
+) {
     val context = LocalContext.current
     DisposableEffect(context) {
         val window = context.findActivity().window
@@ -52,7 +57,33 @@ fun Barcode(
             }
         }
     }
-    Canvas(modifier.onSizeChanged(onSizeChanged)) {
-        drawImage(image ?: return@Canvas)
+    Canvas(modifier) {
+        drawImage(bitmap)
     }
+}
+
+@Composable
+fun rememberBarcodeAsState(
+    color: Color,
+    size: IntSize,
+    code: String,
+    format: BarcodeFormat
+): State<ImageBitmap?> {
+    val image = remember { mutableStateOf(null as ImageBitmap?) }
+    LaunchedEffect(size, code, format, color) {
+        val writer = MultiFormatWriter()
+        image.value = withContext(Dispatchers.Default) {
+            val w = size.width
+            val h = size.height
+            if (w * h <= 0) return@withContext null
+            val matrix = writer.encode(code, format, w, h)
+            val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            for (x in 0 until matrix.width) for (y in 0 until matrix.height) {
+                val pixel = if (matrix.get(x, y)) color.toArgb() else Color.Transparent.toArgb()
+                bitmap.setPixel(x, y, pixel)
+            }
+            bitmap.asImageBitmap()
+        }
+    }
+    return image
 }
