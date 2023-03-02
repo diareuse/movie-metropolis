7f94a49b30bd3533e2abe9c2ea5d89a85d51e163
Viktor De Pasquale <info@depasquale.wiki>
2023-03-02 09:18:38 +0100

    Updated metadata layout

--- app/src/main/java/movie/metropolis/app/screen/detail/component/MovieMetadata.kt
+++ app/src/main/java/movie/metropolis/app/screen/detail/component/MovieMetadata.kt
@@ -1,22 +1,29 @@
 package movie.metropolis.app.screen.detail.component
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.tooling.preview.datasource.*
 import androidx.compose.ui.unit.*
 import movie.metropolis.app.model.ImageView
 import movie.metropolis.app.model.MovieDetailView
 import movie.metropolis.app.presentation.Loadable
+import movie.metropolis.app.presentation.map
 import movie.metropolis.app.presentation.mapNotNull
+import movie.metropolis.app.presentation.onFailure
+import movie.metropolis.app.presentation.onLoading
+import movie.metropolis.app.presentation.onSuccess
+import movie.metropolis.app.screen.detail.ImageViewPreview
+import movie.metropolis.app.screen.detail.MovieDetailViewProvider
 import movie.metropolis.app.screen.listing.component.DefaultPosterAspectRatio
 import movie.style.AppImage
 import movie.style.imagePlaceholder
-import movie.style.layout.CutoutLayout
-import movie.style.modifier.surface
-import movie.style.theme.Theme
-import movie.style.theme.contentColorFor
+import movie.style.layout.PreviewLayout
+import movie.style.textPlaceholder
 
 @Composable
 fun MovieMetadata(
@@ -24,37 +31,63 @@ fun MovieMetadata(
     poster: Loadable<ImageView>,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    MovieMetadataLayout(
         modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(24.dp),
-    ) {
-        val image = poster.getOrNull()
-        val color = image?.spotColor ?: Color.Black
-        val rating = detail.mapNotNull { it.rating }.getOrNull()
-        CutoutLayout(
-            modifier = Modifier
-                .fillMaxWidth(.3f)
-                .aspectRatio(poster.getOrNull()?.aspectRatio ?: DefaultPosterAspectRatio),
-            color = color,
-            shape = Theme.container.poster,
-            overlay = {
-                if (rating != null) Text(
-                    modifier = Modifier
-                        .padding(horizontal = 12.dp, vertical = 8.dp),
-                    text = rating,
-                    color = Theme.color.contentColorFor(color),
-                    style = Theme.textStyle.body
-                )
-            }
-        ) {
-            AppImage(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .imagePlaceholder(image == null)
-                    .surface(Theme.color.container.background),
-                url = image?.url
-            )
+        aspectRatio = poster.getOrNull()?.aspectRatio ?: DefaultPosterAspectRatio,
+        color = poster.getOrNull()?.spotColor ?: Color.Black,
+        rating = {
+            detail
+                .mapNotNull { it.rating }
+                .onLoading {
+                    Text(
+                        "#".repeat(3),
+                        Modifier
+                            .padding(10.dp, 6.dp)
+                            .textPlaceholder(true)
+                    )
+                }
+                .onSuccess { Text(it, Modifier.padding(12.dp, 8.dp)) }
+        },
+        image = {
+            poster
+                .map { it.url }
+                .onLoading {
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .imagePlaceholder(true)
+                    )
+                }
+                .onSuccess { AppImage(it, Modifier.fillMaxSize()) }
+                .onFailure { AppImage(null, Modifier.fillMaxSize()) }
         }
+    ) {
         MovieDetailColumn(detail = detail)
     }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun MovieMetadataPreview(
+    @PreviewParameter(MovieMetadataParameter::class)
+    parameter: MovieMetadataParameter.Data
+) = PreviewLayout {
+    MovieMetadata(parameter.detail, parameter.poster)
+}
+
+class MovieMetadataParameter : CollectionPreviewParameterProvider<MovieMetadataParameter.Data>(
+    listOf(
+        Data(Loadable.loading(), Loadable.loading()),
+        Data(detail = Loadable.loading()),
+        Data(poster = Loadable.loading()),
+        Data(Loadable.failure(Throwable()), Loadable.failure(Throwable())),
+        Data(detail = Loadable.failure(Throwable())),
+        Data(poster = Loadable.failure(Throwable())),
+    )
+) {
+    data class Data(
+        val detail: Loadable<MovieDetailView> = Loadable.success(MovieDetailViewProvider().values.first()),
+        val poster: Loadable<ImageView> = Loadable.success(ImageViewPreview())
+    )
 }
---
+++ app/src/main/java/movie/metropolis/app/screen/detail/component/MovieMetadataLayout.kt
@@ -0,0 +1,78 @@
+package movie.metropolis.app.screen.detail.component
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.*
+import movie.metropolis.app.screen.listing.component.DefaultPosterAspectRatio
+import movie.style.layout.CutoutLayout
+import movie.style.layout.PreviewLayout
+import movie.style.theme.Theme
+import movie.style.theme.contentColorFor
+
+@Composable
+fun MovieMetadataLayout(
+    aspectRatio: Float,
+    color: Color,
+    rating: @Composable () -> Unit,
+    image: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(24.dp),
+    ) {
+        CutoutLayout(
+            modifier = Modifier
+                .fillMaxWidth(.3f)
+                .aspectRatio(aspectRatio),
+            color = color,
+            shape = Theme.container.poster,
+            overlay = {
+                CompositionLocalProvider(
+                    LocalContentColor provides Theme.color.contentColorFor(color),
+                    LocalTextStyle provides Theme.textStyle.body
+                ) {
+                    rating()
+                }
+            }
+        ) {
+            image()
+        }
+        Box(Modifier.weight(1f)) {
+            content()
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+private fun MovieMetadataLayoutPreview() = PreviewLayout {
+    MovieMetadataLayout(
+        aspectRatio = DefaultPosterAspectRatio,
+        color = Color.Magenta,
+        rating = { Text("75%", Modifier.padding(12.dp, 8.dp)) },
+        image = {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Magenta)
+            )
+        }
+    ) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(Color.Gray)
+        )
+    }
+}
