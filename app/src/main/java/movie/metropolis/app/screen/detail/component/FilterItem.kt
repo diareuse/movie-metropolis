8cbaef4c461d14a27fbf623b9d7e582c2d92d4ad
Viktor De Pasquale <hello@depasquale.wiki>
2023-05-13 14:49:59 +0200

    Updated for use of material components

--- app/src/main/java/movie/metropolis/app/screen/detail/component/FilterItem.kt
+++ app/src/main/java/movie/metropolis/app/screen/detail/component/FilterItem.kt
@@ -2,62 +2,59 @@ package movie.metropolis.app.screen.detail.component
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.*
+import androidx.compose.foundation.shape.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.platform.*
-import androidx.compose.ui.res.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.tooling.preview.datasource.*
-import androidx.compose.ui.unit.*
-import movie.metropolis.app.R
 import movie.metropolis.app.model.Filter
-import movie.style.AppButton
 import movie.style.layout.PreviewLayout
 import movie.style.theme.Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FilterItem(
     filter: Filter,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    AppButton(
+    FilterChip(
         modifier = modifier,
+        selected = filter.isSelected,
         onClick = onClick,
-        containerColor = when (filter.isSelected) {
-            true -> Theme.color.container.tertiary
-            else -> Theme.color.container.surface
-        },
-        contentColor = when (filter.isSelected) {
-            true -> Theme.color.content.tertiary
-            else -> Theme.color.content.surface
-        },
-        contentPadding = PaddingValues(16.dp, 8.dp),
-        elevation = 0.dp
-    ) {
-        val style = Theme.textStyle.body
-        if (filter.isSelected) {
-            Icon(
-                modifier = Modifier.size(with(LocalDensity.current) { style.fontSize.toDp() }),
-                painter = painterResource(R.drawable.ic_selected),
-                contentDescription = null
-            )
-            Spacer(Modifier.width(8.dp))
+        label = { Text(filter.value) },
+        colors = FilterChipDefaults.filterChipColors(
+            containerColor = Theme.color.container.surface,
+            labelColor = Theme.color.content.surface,
+            iconColor = Theme.color.content.surface,
+            selectedContainerColor = Theme.color.container.tertiary,
+            selectedLabelColor = Theme.color.content.tertiary,
+            selectedLeadingIconColor = Theme.color.content.tertiary,
+            selectedTrailingIconColor = Theme.color.content.tertiary
+        ),
+        shape = CircleShape,
+        border = null,
+        leadingIcon = {
+            AnimatedVisibility(
+                visible = filter.isSelected,
+                enter = expandIn(expandFrom = Alignment.CenterStart),
+                exit = shrinkOut(shrinkTowards = Alignment.CenterStart)
+            ) {
+                Icon(Icons.Rounded.Check, null)
+            }
         }
-        Text(
-            text = filter.value,
-            style = style
-        )
-    }
+    )
 }
 
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Composable
 private fun FilterItemPreview(
-    @PreviewParameter(FilterParameter::class, 1)
+    @PreviewParameter(FilterParameter::class)
     parameter: Filter
 ) = PreviewLayout {
     FilterItem(parameter, {})
