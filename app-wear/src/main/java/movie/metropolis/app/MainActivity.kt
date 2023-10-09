efdb4850b8199465c4b1f488e2dd773f6d94c510
Viktor De Pasquale <hello@depasquale.wiki>
2023-10-09 12:30:59 +0200

    Added splash screen

--- app-wear/src/main/java/movie/metropolis/app/MainActivity.kt
+++ app-wear/src/main/java/movie/metropolis/app/MainActivity.kt
@@ -3,6 +3,7 @@ package movie.metropolis.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import movie.metropolis.app.screen.Navigation
 import movie.style.theme.Theme
@@ -10,6 +11,7 @@ import movie.style.theme.Theme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             Theme(darkTheme = true) {
