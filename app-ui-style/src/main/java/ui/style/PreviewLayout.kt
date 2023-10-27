8b8528026e8f2ea547b95fd34ac6646a60960cfb
Viktor De Pasquale <hello@depasquale.wiki>
2023-10-27 15:17:54 +0200

    Removed dependency on deprecated style package

---
+++ app-ui-style/src/main/java/ui/style/PreviewLayout.kt
@@ -0,0 +1,12 @@
+package ui.style
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+
+@Composable
+fun PreviewLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    content()
+}
