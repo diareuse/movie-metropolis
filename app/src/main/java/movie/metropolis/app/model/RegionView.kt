033ede769b69ca5124f9c054e4c8858e9920b520
Viktor De Pasquale <hello@depasquale.wiki>
2023-10-30 09:34:05 +0100

    Added region setup screen

--- app/src/main/java/movie/metropolis/app/model/RegionView.kt
+++ app/src/main/java/movie/metropolis/app/model/RegionView.kt
@@ -1,10 +1,14 @@
 package movie.metropolis.app.model
 
+import androidx.annotation.DrawableRes
 import androidx.compose.runtime.*
 
 @Immutable
 interface RegionView {
 
     val name: String
+
+    @get:DrawableRes
+    val icon: Int
 
 }
