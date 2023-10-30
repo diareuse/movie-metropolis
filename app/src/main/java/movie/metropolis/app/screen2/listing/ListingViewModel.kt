ea2e81297e52250cd91c7997b963cdcd9b9175a5
Viktor De Pasquale <hello@depasquale.wiki>
2023-10-30 19:02:43 +0100

    Added promo pager

--- app/src/main/java/movie/metropolis/app/screen2/listing/ListingViewModel.kt
+++ app/src/main/java/movie/metropolis/app/screen2/listing/ListingViewModel.kt
@@ -6,6 +6,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
 import movie.metropolis.app.presentation.listing.ListingFacade
 import movie.metropolis.app.presentation.listing.ListingFacade.Companion.groups
+import movie.metropolis.app.presentation.listing.ListingFacade.Companion.promotions
 import movie.metropolis.app.presentation.mapLoadable
 import movie.metropolis.app.util.retainStateIn
 import javax.inject.Inject
@@ -16,6 +17,10 @@ class ListingViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val facade = factory.current()
+
+    val promotions = facade.promotions
+        .map { it.getOrNull().orEmpty() }
+        .retainStateIn(viewModelScope, emptyList())
 
     val movies = facade.groups
         .mapLoadable { it.values.flatten().distinctBy { it.id } }
