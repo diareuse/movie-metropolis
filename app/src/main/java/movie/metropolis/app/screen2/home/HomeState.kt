2a14e994719b641f5d1a59eefe0a539321b7420b
Viktor De Pasquale <hello@depasquale.wiki>
2023-10-31 09:47:24 +0100

    Updated order of bottom nav items

--- app/src/main/java/movie/metropolis/app/screen2/home/HomeState.kt
+++ app/src/main/java/movie/metropolis/app/screen2/home/HomeState.kt
@@ -17,15 +17,15 @@ enum class HomeState(
         inactive = R.drawable.ic_listing_inactive,
         title = R.string.home_listing_title
     ),
-    Tickets(
-        active = R.drawable.ic_ticket_active,
-        inactive = R.drawable.ic_ticket_inactive,
-        title = R.string.home_tickets_title
-    ),
     Cinemas(
         active = R.drawable.ic_cinemas_active,
         inactive = R.drawable.ic_cinemas_inactive,
         title = R.string.home_cinemas_title
+    ),
+    Tickets(
+        active = R.drawable.ic_ticket_active,
+        inactive = R.drawable.ic_ticket_inactive,
+        title = R.string.home_tickets_title
     ),
     Profile(
         active = R.drawable.ic_profile_active,
