b911a4ceaf614fd08c769197ba03610cdb61835e
Viktor De Pasquale <hello@depasquale.wiki>
2023-10-31 18:15:09 +0100

    Added storable home tab

--- app/src/main/java/movie/metropolis/app/util/Storeable.kt
+++ app/src/main/java/movie/metropolis/app/util/Storeable.kt
@@ -7,12 +7,14 @@ import androidx.compose.runtime.saveable.*
 import androidx.compose.ui.platform.*
 import androidx.core.content.edit
 
+private const val UiStore = "ui-store"
+
 @Composable
 fun rememberStoreable(
     key: String,
     default: Float
 ): MutableState<Float> {
-    val prefs = rememberSharedPrefs(name = "ui-store")
+    val prefs = rememberSharedPrefs(UiStore)
     val state = rememberSaveable {
         mutableFloatStateOf(prefs.getFloat(key, default))
     }
@@ -20,6 +22,31 @@ fun rememberStoreable(
         prefs.edit { putFloat(key, state.value) }
     }
     return state
+}
+
+@Composable
+fun rememberStoreable(
+    key: String,
+    default: String?
+): MutableState<String> {
+    val prefs = rememberSharedPrefs(UiStore)
+    val state = rememberSaveable {
+        mutableStateOf(prefs.getString(key, default).orEmpty())
+    }
+    LaunchedEffect(state.value) {
+        prefs.edit { putString(key, state.value) }
+    }
+    return state
+}
+
+@Composable
+fun getStoreable(key: String, default: String, value: () -> String): String {
+    val prefs = rememberSharedPrefs(UiStore)
+    val newValue by remember(value) { derivedStateOf(value) }
+    LaunchedEffect(newValue) {
+        prefs.edit { putString(key, newValue) }
+    }
+    return remember(prefs, key) { prefs.getString(key, default) ?: default }
 }
 
 @Composable
