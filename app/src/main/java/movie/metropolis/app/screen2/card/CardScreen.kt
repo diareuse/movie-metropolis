24f7baa1b7b060d083a1f85bc04af322e8021d4c
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-02 09:11:48 +0100

    Added credentials on the front side of the card

--- app/src/main/java/movie/metropolis/app/screen2/card/CardScreen.kt
+++ app/src/main/java/movie/metropolis/app/screen2/card/CardScreen.kt
@@ -1,7 +1,6 @@
 package movie.metropolis.app.screen2.card
 
-import androidx.compose.animation.*
-import androidx.compose.animation.core.*
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
@@ -10,7 +9,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.colorspace.*
 import androidx.compose.ui.input.pointer.*
 import androidx.compose.ui.layout.*
 import androidx.compose.ui.platform.*
@@ -18,10 +16,11 @@ import androidx.compose.ui.res.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.*
 import com.google.zxing.BarcodeFormat
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import movie.metropolis.app.R
+import movie.metropolis.app.model.CinemaSimpleView
 import movie.metropolis.app.model.MembershipView
+import movie.metropolis.app.model.UserView
 import movie.metropolis.app.screen.profile.component.MembershipViewParameter
 import movie.metropolis.app.screen2.card.component.CardContentBack
 import movie.metropolis.app.screen2.card.component.CardContentFront
@@ -30,32 +29,45 @@ import movie.style.Barcode
 import movie.style.layout.PreviewLayout
 import movie.style.modifier.surface
 import movie.style.modifier.vertical
+import movie.style.theme.Theme
 
 private val CinemaCityColor = Color(0xFFE78838)
 
 @Composable
 fun CardScreen(
+    user: UserView,
     membership: MembershipView,
+    onCloseRequest: () -> Unit,
     modifier: Modifier = Modifier,
     state: CardScreenState = remember { CardScreenState() },
 ) {
     val scope = rememberCoroutineScope()
     val density = LocalDensity.current
+    BackHandler {
+        onCloseRequest()
+    }
     Box(
         modifier = modifier
             .fillMaxSize()
             .background(state.background)
             .onSizeChanged { state.updateOffset(with(density) { it.height.toDp() }) }
             .pointerInput(Unit) {
-                detectHorizontalDragGestures(
-                    onDragCancel = { scope.launch { state.settle() } },
-                    onDragEnd = { scope.launch { state.settle() } }
-                ) { change, amount ->
-                    change.consume()
-                    val amount = if (amount < 0) 360 + amount else amount
-                    state.rotation += amount
-                    if (state.rotation > 360f)
-                        state.rotation -= 360f
+                scope.launch {
+                    detectHorizontalDragGestures(
+                        onDragCancel = { scope.launch { state.settle() } },
+                        onDragEnd = { scope.launch { state.settle() } }
+                    ) { change, amount ->
+                        change.consume()
+                        val amount = if (amount < 0) 360 + amount else amount
+                        state.rotation += amount
+                        if (state.rotation > 360f)
+                            state.rotation -= 360f
+                    }
+                }
+                scope.launch {
+                    detectTapGestures {
+                        onCloseRequest()
+                    }
                 }
             },
         contentAlignment = Alignment.Center
@@ -64,7 +76,8 @@ fun CardScreen(
             Image(
                 painter = painterResource(id = R.drawable.ic_logo_cinemacity),
                 contentDescription = null,
-                colorFilter = ColorFilter.tint(CinemaCityColor)
+                colorFilter = ColorFilter.tint(CinemaCityColor),
+                contentScale = ContentScale.FillHeight
             )
         }
         val name = @Composable {
@@ -93,12 +106,31 @@ fun CardScreen(
                 }
             },
             front = {
-                CardContentFront(logo = logo, name = name)
+                CardContentFront(logo = logo, name = name, cardholder = {
+                    CompositionLocalProvider(
+                        LocalContentColor provides LocalContentColor.current.copy(alpha = .7f)
+                    ) {
+                        Text(stringResource(id = R.string.points_count, membership.points))
+                        when {
+                            membership.isExpired -> Text(stringResource(R.string.expired))
+                            else -> Text(
+                                stringResource(R.string.expires_at, membership.daysRemaining)
+                            )
+                        }
+
+                    }
+                    Text(
+                        "%s %s".format(user.firstName, user.lastName),
+                        style = Theme.textStyle.title
+                    )
+                })
             },
             back = {
                 CardContentBack(logo = logo, name = name, code = {
                     Barcode(
-                        modifier = Modifier.background(Color.White),
+                        modifier = Modifier
+                            .surface(Color.White, Theme.container.poster)
+                            .padding(vertical = 8.dp),
                         code = membership.cardNumber,
                         format = BarcodeFormat.CODE_128
                     )
@@ -108,106 +140,45 @@ fun CardScreen(
     }
 }
 
-class CardScreenState {
-
-    var rotation by mutableFloatStateOf(0f)
-    var blur by mutableStateOf(0.dp)
-        private set
-    var background by mutableStateOf(Color.Transparent)
-        private set
-    var offset by mutableStateOf(Int.MAX_VALUE.dp)
-        private set
-    private var closedOffset by mutableStateOf(Int.MAX_VALUE.dp)
-    val isOpen: Boolean
-        get() = offset != closedOffset
-
-    fun updateOffset(offset: Dp) {
-        if (closedOffset == Int.MAX_VALUE.dp) {
-            this.offset = offset
-            this.closedOffset = offset
-        }
-    }
-
-    suspend fun settle() {
-        val to = when (rotation % 360) {
-            in 90f..<270f -> 180f
-            in 270f..360f -> 360f
-            else -> 0f
-        }
-        animate(rotation, to, animationSpec = tween(700)) { it, _ ->
-            rotation = it
-        }
-    }
-
-    suspend fun close() {
-        coroutineScope {
-            launch {
-                animate(offset.value, closedOffset.value, animationSpec = tween(1000)) { it, _ ->
-                    offset = it.dp
-                }
-            }
-            launch {
-                animate(rotation, rotation + 180f, animationSpec = tween(700)) { it, _ ->
-                    rotation = it
-                }
-                rotation = 0f
-            }
-            launch {
-                animate(blur.value, 0f, animationSpec = tween(700)) { it, _ ->
-                    blur = it.dp
-                }
-            }
-            launch {
-                animate(
-                    typeConverter = Color.VectorConverter(ColorSpaces.Srgb),
-                    background,
-                    Color.Transparent,
-                    animationSpec = tween(1000)
-                ) { it, _ ->
-                    background = it
-                }
-            }
-        }
-    }
-
-    suspend fun open() {
-        coroutineScope {
-            launch {
-                animate(offset.value, 0f, animationSpec = tween(700)) { it, _ ->
-                    offset = it.dp
-                }
-            }
-            launch {
-                animate(blur.value, 16f, animationSpec = tween(700)) { it, _ ->
-                    blur = it.dp
-                }
-            }
-            launch {
-                animate(180f, 360f, animationSpec = tween(1400)) { it, _ ->
-                    rotation = it
-                }
-                rotation %= 360f
-            }
-            launch {
-                animate(
-                    typeConverter = Color.VectorConverter(ColorSpaces.Srgb),
-                    background,
-                    Color.Black.copy(alpha = .5f),
-                    animationSpec = tween(700)
-                ) { it, _ ->
-                    background = it
-                }
-            }
-        }
-    }
-
-}
-
 @Preview
 @Composable
 private fun CardScreenPreview() = PreviewLayout {
     val membership = remember { MembershipViewParameter().values.first() }
+    val user = remember { UserViewParameter().values.first() }
     val state = remember { CardScreenState().also { it.updateOffset(0.dp) } }
-    CardScreen(membership, state = state)
+    CardScreen(user, membership, state = state, onCloseRequest = {})
+}
 
+@Preview
+@Composable
+private fun CardScreenBackPreview() = PreviewLayout {
+    val membership = remember { MembershipViewParameter().values.first() }
+    val user = remember { UserViewParameter().values.first() }
+    val state = remember {
+        CardScreenState().also {
+            it.updateOffset(0.dp)
+            it.rotation = 180f
+        }
+    }
+    CardScreen(user, membership, state = state, onCloseRequest = {})
+}
+
+class UserViewParameter : PreviewParameterProvider<UserView> {
+    override val values = sequence<UserView> {
+        yield(Data())
+    }
+
+    private data class Data(
+        override val firstName: String = "John",
+        override val lastName: String = "Doe",
+        override val email: String = "joh.doe@email.com",
+        override val phone: String = "+1 234 2344233",
+        override val favorite: CinemaSimpleView? = null,
+        override val consent: UserView.ConsentView = Consent()
+    ) : UserView
+
+    private data class Consent(
+        override val marketing: Boolean = false,
+        override val premium: Boolean = false
+    ) : UserView.ConsentView
 }
---
+++ app/src/main/java/movie/metropolis/app/screen2/card/CardScreenState.kt
@@ -0,0 +1,105 @@
+package movie.metropolis.app.screen2.card
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.colorspace.*
+import androidx.compose.ui.unit.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class CardScreenState {
+
+    var rotation by mutableFloatStateOf(0f)
+    var blur by mutableStateOf(0.dp)
+        private set
+    var background by mutableStateOf(Color.Transparent)
+        private set
+    var offset by mutableStateOf(Int.MAX_VALUE.dp)
+        private set
+    private var closedOffset by mutableStateOf(Int.MAX_VALUE.dp)
+    val isOpen: Boolean
+        get() = offset != closedOffset
+
+    fun updateOffset(offset: Dp) {
+        if (closedOffset == Int.MAX_VALUE.dp) {
+            this.offset = offset
+            this.closedOffset = offset
+        }
+    }
+
+    suspend fun settle() {
+        val to = when (rotation % 360) {
+            in 90f..<270f -> 180f
+            in 270f..360f -> 360f
+            else -> 0f
+        }
+        animate(rotation, to, animationSpec = tween(700)) { it, _ ->
+            rotation = it
+        }
+    }
+
+    suspend fun close() {
+        coroutineScope {
+            launch {
+                animate(offset.value, closedOffset.value, animationSpec = tween(1000)) { it, _ ->
+                    offset = it.dp
+                }
+            }
+            launch {
+                animate(rotation, rotation + 180f, animationSpec = tween(700)) { it, _ ->
+                    rotation = it
+                }
+                rotation = 0f
+            }
+            launch {
+                animate(blur.value, 0f, animationSpec = tween(700)) { it, _ ->
+                    blur = it.dp
+                }
+            }
+            launch {
+                animate(
+                    typeConverter = Color.VectorConverter(ColorSpaces.Srgb),
+                    background,
+                    Color.Transparent,
+                    animationSpec = tween(1000)
+                ) { it, _ ->
+                    background = it
+                }
+            }
+        }
+    }
+
+    suspend fun open() {
+        coroutineScope {
+            launch {
+                animate(offset.value, 0f, animationSpec = tween(700)) { it, _ ->
+                    offset = it.dp
+                }
+            }
+            launch {
+                animate(blur.value, 16f, animationSpec = tween(700)) { it, _ ->
+                    blur = it.dp
+                }
+            }
+            launch {
+                animate(180f, 360f, animationSpec = tween(1400)) { it, _ ->
+                    rotation = it
+                }
+                rotation %= 360f
+            }
+            launch {
+                animate(
+                    typeConverter = Color.VectorConverter(ColorSpaces.Srgb),
+                    background,
+                    Color.Black.copy(alpha = .5f),
+                    animationSpec = tween(700)
+                ) { it, _ ->
+                    background = it
+                }
+            }
+        }
+    }
+
+}
