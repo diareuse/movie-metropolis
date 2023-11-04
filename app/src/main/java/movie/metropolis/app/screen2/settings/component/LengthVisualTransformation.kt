86c591888ece66acb9240fd7b7cfc954a054c5b9
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-04 21:08:36 +0100

    Added settings screen implementation

---
+++ app/src/main/java/movie/metropolis/app/screen2/settings/component/LengthVisualTransformation.kt
@@ -0,0 +1,22 @@
+package movie.metropolis.app.screen2.settings.component
+
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.input.*
+
+object LengthVisualTransformation : VisualTransformation {
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        return TransformedText(text + AnnotatedString(" km"), CeilOffsetMapping(text.length))
+    }
+
+    class CeilOffsetMapping(private val ceil: Int) : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            return offset
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            return offset.coerceAtMost(ceil)
+        }
+    }
+
+}
