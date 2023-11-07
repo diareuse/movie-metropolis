7028425bd9380869ea4bcb67cb75925ed6d5d199
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-07 11:28:22 +0100

    Added formatted date

--- app/src/main/java/movie/metropolis/app/model/LazyTimeView.kt
+++ app/src/main/java/movie/metropolis/app/model/LazyTimeView.kt
@@ -1,9 +1,18 @@
 package movie.metropolis.app.model
 
 import kotlinx.coroutines.flow.Flow
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 interface LazyTimeView {
+
     val date: Date
     val content: Flow<List<TimeView>>
+    val dateString get() = format.format(date)
+
+    companion object {
+        private val format = SimpleDateFormat("dd\nMMM", Locale.getDefault())
+    }
+
 }
