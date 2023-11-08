ce6e008e6e265670630a8cc1cdb895212623f4f8
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-08 12:26:35 +0100

    Added purchase complete screen

---
+++ app/src/main/java/movie/metropolis/app/screen2/purchase/PurchaseCompleteViewModel.kt
@@ -0,0 +1,21 @@
+package movie.metropolis.app.screen2.purchase
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import movie.metropolis.app.presentation.order.OrderCompleteFacade
+import movie.metropolis.app.presentation.order.OrderCompleteFacade.Companion.productsFlow
+import movie.metropolis.app.util.retainStateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class PurchaseCompleteViewModel @Inject constructor(
+    facade: OrderCompleteFacade
+) : ViewModel() {
+
+    val products = facade.productsFlow
+        .map { it.getOrNull().orEmpty() }
+        .retainStateIn(viewModelScope, emptyList())
+
+}
