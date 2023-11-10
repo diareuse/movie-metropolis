4f940f9d3553a2a8af961c1bb596f6e065b650fc
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-10 13:59:11 +0100

    Added two-pane surface to movie detail

--- app/src/main/java/movie/metropolis/app/screen2/movie/MovieScreen.kt
+++ app/src/main/java/movie/metropolis/app/screen2/movie/MovieScreen.kt
@@ -4,7 +4,6 @@ package movie.metropolis.app.screen2.movie
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.automirrored.rounded.*
@@ -12,7 +11,6 @@ import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.input.nestedscroll.*
 import androidx.compose.ui.text.style.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.*
@@ -23,6 +21,7 @@ import movie.metropolis.app.screen2.movie.component.LargeRatingBox
 import movie.style.BackgroundImage
 import movie.style.CollapsingTopAppBar
 import movie.style.Image
+import movie.style.TwoPaneSurface
 import movie.style.layout.PreviewLayout
 import movie.style.layout.alignForLargeScreen
 import movie.style.rememberPaletteImageState
@@ -61,77 +60,78 @@ fun MovieScreen(
         modifier = Modifier.fillMaxSize(),
         state = poster
     )
-    // fixme this column could use a content shifting strategy based on screen size
-    Column(
-        modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection)
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(padding)
-            .alignForLargeScreen(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        LargeMoviePoster(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .padding(top = 24.dp),
-            color = poster.palette.color,
-            contentColor = poster.palette.textColor,
-            order = { Icon(Icons.Rounded.ShoppingCart, null) },
-            rating = {
-                if (movie?.rating != null) LargeRatingBox(
-                    color = poster.palette.color,
-                    contentColor = poster.palette.textColor
-                ) {
-                    Text(movie.rating.orEmpty())
-                }
-            },
-            onOrderClick = onBookClick
-        ) {
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                state = poster
-            )
+    TwoPaneSurface(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = padding,
+        connection = scrollBehavior.nestedScrollConnection,
+        primaryWeight = .75f,
+        primary = {
+            LargeMoviePoster(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                    .padding(top = 24.dp),
+                color = poster.palette.color,
+                contentColor = poster.palette.textColor,
+                order = { Icon(Icons.Rounded.ShoppingCart, null) },
+                rating = {
+                    if (movie?.rating != null) LargeRatingBox(
+                        color = poster.palette.color,
+                        contentColor = poster.palette.textColor
+                    ) {
+                        Text(movie.rating.orEmpty())
+                    }
+                },
+                onOrderClick = onBookClick
+            ) {
+                Image(
+                    modifier = Modifier.fillMaxWidth(),
+                    state = poster
+                )
+            }
+        },
+        secondary = {
+            if (movie != null) Column(
+                modifier = Modifier
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    text = movie.name,
+                    style = Theme.textStyle.title,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "%s • %s • %s".format(
+                        movie.duration,
+                        movie.countryOfOrigin,
+                        movie.releasedAt
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                if (movie.directors.isNotEmpty()) Text(
+                    text = movie.directors.joinToString(),
+                    style = Theme.textStyle.emphasis,
+                    textAlign = TextAlign.Center
+                )
+                if (movie.cast.isNotEmpty()) Text(
+                    text = movie.cast.joinToString(),
+                    style = Theme.textStyle.caption,
+                    textAlign = TextAlign.Center
+                )
+                if (movie.description.isNotEmpty()) Text(
+                    text = movie.description,
+                    textAlign = TextAlign.Justify
+                )
+            }
         }
-        if (movie != null) Column(
-            modifier = Modifier
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Text(
-                text = movie.name,
-                style = Theme.textStyle.title,
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = "%s • %s • %s".format(
-                    movie.duration,
-                    movie.countryOfOrigin,
-                    movie.releasedAt
-                ),
-                textAlign = TextAlign.Center
-            )
-            if (movie.directors.isNotEmpty()) Text(
-                text = movie.directors.joinToString(),
-                style = Theme.textStyle.emphasis,
-                textAlign = TextAlign.Center
-            )
-            if (movie.cast.isNotEmpty()) Text(
-                text = movie.cast.joinToString(),
-                style = Theme.textStyle.caption,
-                textAlign = TextAlign.Center
-            )
-            if (movie.description.isNotEmpty()) Text(
-                text = movie.description,
-                textAlign = TextAlign.Justify
-            )
-        }
-    }
+    )
 }
 
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Preview(showBackground = true, widthDp = 1000)
 @Composable
 private fun MovieScreenPreview() = PreviewLayout {
     val movie = MovieDetailViewProvider().values.first()
--- style/src/main/java/movie/style/TwoPaneSurface.kt
+++ style/src/main/java/movie/style/TwoPaneSurface.kt
@@ -2,85 +2,86 @@ package movie.style
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.*
 import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.input.nestedscroll.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.*
 import movie.style.layout.PreviewLayout
+import movie.style.layout.rememberWindowSizeClass
 
 @Composable
 fun TwoPaneSurface(
-    primary: @Composable () -> Unit,
-    secondary: @Composable () -> Unit,
+    primary: @Composable ColumnScope.() -> Unit,
+    secondary: @Composable ColumnScope.() -> Unit,
     modifier: Modifier = Modifier,
-    separator: @Composable () -> Unit = { TwoPaneSurfaceDefaults.Separator() },
-    sizeClass: WindowWidthSizeClass = LocalWindowSizeClass.current.widthSizeClass
-) = when (sizeClass) {
-    WindowWidthSizeClass.Expanded -> TwoPaneSurface(
-        primary = primary,
-        secondary = secondary,
-        separator = separator,
+    connection: NestedScrollConnection? = null,
+    verticalAlignment: Alignment.Vertical = Alignment.Top,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    primaryWeight: Float = 1f,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) = when (rememberWindowSizeClass().widthSizeClass) {
+    WindowWidthSizeClass.Compact -> Column(
         modifier = modifier
-    )
-
-    else -> SinglePaneSurface(
-        primary = primary,
-        modifier = modifier
-    )
-}
-
-@Composable
-private fun TwoPaneSurface(
-    primary: @Composable () -> Unit,
-    secondary: @Composable () -> Unit,
-    separator: @Composable () -> Unit,
-    modifier: Modifier = Modifier,
-) = Row(modifier = modifier) {
-    Box(modifier = Modifier.weight(1f), propagateMinConstraints = true) {
+            .run { if (connection != null) nestedScroll(connection) else this }
+            .verticalScroll(rememberScrollState())
+            .padding(contentPadding),
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment
+    ) {
         primary()
-    }
-    separator()
-    Box(modifier = Modifier.weight(1f), propagateMinConstraints = true) {
         secondary()
     }
-}
 
-@Composable
-private fun SinglePaneSurface(
-    primary: @Composable () -> Unit,
-    modifier: Modifier = Modifier,
-) = Box(modifier = modifier, propagateMinConstraints = true) {
-    primary()
-}
-
-object TwoPaneSurfaceDefaults {
-
-    @Composable
-    fun Separator(
-        modifier: Modifier = Modifier,
-        color: Color = LocalContentColor.current.copy(alpha = .18f),
-        width: Dp = 1.dp,
-        shape: Shape = CircleShape
+    else -> Row(
+        modifier = modifier,
+        verticalAlignment = verticalAlignment,
+        horizontalArrangement = horizontalArrangement
     ) {
-        Box(
-            modifier
-                .fillMaxHeight()
-                .width(width)
-                .statusBarsPadding()
-                .navigationBarsPadding()
-                .background(color, shape)
-        )
+        Column(
+            modifier = Modifier
+                .weight(primaryWeight)
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding),
+            verticalArrangement = verticalArrangement,
+            horizontalAlignment = horizontalAlignment
+        ) {
+            primary()
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .run { if (connection != null) nestedScroll(connection) else this }
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding),
+            verticalArrangement = verticalArrangement,
+            horizontalAlignment = horizontalAlignment
+        ) {
+            secondary()
+        }
     }
-
 }
 
 @Preview(showBackground = true)
 @Preview(showBackground = true, widthDp = 1000)
 @Composable
 private fun TwoPaneSurfacePreview() = PreviewLayout {
-    TwoPaneSurface({ Text("A") }, { Text("B") })
+    TwoPaneSurface(
+        primaryWeight = .5f,
+        primary = {
+            Box(
+                Modifier
+                    .aspectRatio(0.67f, false)
+                    .background(Brush.radialGradient(listOf(Color.Blue, Color.Red)))
+            )
+        },
+        secondary = {
+            Text("B", modifier = Modifier.fillMaxWidth())
+        }
+    )
 }
