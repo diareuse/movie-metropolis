59f59d0a77f879c73da55747eb07cceb948a3a43
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-14 14:22:19 +0100

    Fixed fetching visible items and optimized performance

--- app/src/main/java/movie/metropolis/app/util/LayoutInfo.kt
+++ app/src/main/java/movie/metropolis/app/util/LayoutInfo.kt
@@ -1,45 +1,54 @@
 package movie.metropolis.app.util
 
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.lazy.staggeredgrid.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.*
 import androidx.compose.ui.util.*
 
+private operator fun IntRange.contains(other: IntRange): Boolean {
+    return first <= other.first && last >= other.last
+}
+
 val LazyStaggeredGridLayoutInfo.completelyVisibleItemsInfo: List<LazyStaggeredGridItemInfo>
     get() {
+        fun LazyStaggeredGridItemInfo.offset() = when (orientation) {
+            Orientation.Vertical -> offset.y..offset.y + size.height
+            Orientation.Horizontal -> offset.x..offset.x + size.width
+        }
+
         val visibleOffset = viewportStartOffset..viewportEndOffset
-        return visibleItemsInfo.filter { it.offset.x + it.size.width * 2 in visibleOffset }
+        return visibleItemsInfo.fastFilter { it.offset() in visibleOffset }
     }
 
 val LazyListLayoutInfo.completelyVisibleItemsInfo: List<LazyListItemInfo>
     get() {
         val visibleOffset = viewportStartOffset..viewportEndOffset
-        return visibleItemsInfo.filter { it.offset + it.size * 2 in visibleOffset }
+        fun LazyListItemInfo.offset() = offset + size
+        return visibleItemsInfo.fastFilter { it.offset() in visibleOffset }
     }
 
 @Composable
 fun LazyListState.rememberVisibleItemAsState(): State<Int> {
-    val layoutInfo by remember { derivedStateOf { layoutInfo } }
-    fun select() = layoutInfo.completelyVisibleItemsInfo.randomOrNull()?.index
-    val item = rememberSaveable { mutableIntStateOf(select() ?: -1) }
-    LaunchedEffect(layoutInfo) {
-        val info = layoutInfo.completelyVisibleItemsInfo
-        if (item.value !in info.fastMap { it.index })
-            item.value = info.randomOrNull()?.index ?: -1
+    val visibleItems by remember { derivedStateOf { layoutInfo.completelyVisibleItemsInfo.fastMap { it.index } } }
+    val item = rememberSaveable { mutableIntStateOf(visibleItems.randomOrNull() ?: -1) }
+    LaunchedEffect(item.value !in visibleItems) {
+        val info = visibleItems
+        if (item.value !in info)
+            item.value = info.randomOrNull() ?: -1
     }
     return item
 }
 
 @Composable
 fun LazyStaggeredGridState.rememberVisibleItemAsState(): State<Int> {
-    val layoutInfo by remember { derivedStateOf { layoutInfo } }
-    fun select() = layoutInfo.completelyVisibleItemsInfo.randomOrNull()?.index
-    val item = rememberSaveable { mutableIntStateOf(select() ?: -1) }
-    LaunchedEffect(layoutInfo) {
-        val info = layoutInfo.completelyVisibleItemsInfo
-        if (item.value !in info.fastMap { it.index })
-            item.value = info.randomOrNull()?.index ?: -1
+    val visibleItems by remember { derivedStateOf { layoutInfo.completelyVisibleItemsInfo.fastMap { it.index } } }
+    val item = rememberSaveable { mutableIntStateOf(visibleItems.randomOrNull() ?: -1) }
+    LaunchedEffect(item.value !in visibleItems) {
+        val info = visibleItems
+        if (item.value !in info)
+            item.value = info.randomOrNull() ?: -1
     }
     return item
 }
