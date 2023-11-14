1b3b9d9dafed1573a4b93b5d438139440cad8ec7
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-14 14:22:00 +0100

    Updated image loading to be more efficient

--- style/src/main/java/movie/style/image/ImageRequest.kt
+++ style/src/main/java/movie/style/image/ImageRequest.kt
@@ -6,10 +6,7 @@ import coil.request.CachePolicy
 import coil.request.ImageRequest
 import coil.size.Precision
 import coil.size.Scale
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @Composable
 fun rememberImageRequest(url: Any?): ImageRequest {
     val context = LocalContext.current
@@ -18,10 +15,6 @@ fun rememberImageRequest(url: Any?): ImageRequest {
         ImageRequest.Builder(context)
             .data(url)
             .diskCachePolicy(CachePolicy.ENABLED)
-            .decoderDispatcher(Dispatchers.Default)
-            .fetcherDispatcher(Dispatchers.IO.limitedParallelism(5))
-            .interceptorDispatcher(Dispatchers.IO.limitedParallelism(5))
-            .transformationDispatcher(Dispatchers.Default)
             .lifecycle(owner)
             .precision(Precision.INEXACT)
             .scale(Scale.FILL)
