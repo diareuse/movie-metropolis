ba1569a87f1dd747000602cc69361f925cff67c9
Viktor De Pasquale <hello@depasquale.wiki>
2023-11-17 08:27:09 +0100

    Added arbitrary delays to startup

--- app-wear/src/main/java/movie/metropolis/app/MainActivity.kt
+++ app-wear/src/main/java/movie/metropolis/app/MainActivity.kt
@@ -1,8 +1,13 @@
 package movie.metropolis.app
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.os.postDelayed
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import movie.metropolis.app.screen.Navigation
@@ -13,6 +18,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        var isReady = false
+        Handler(Looper.getMainLooper()).postDelayed(500) { isReady = true }
+        val content = findViewById<View>(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw() = when {
+                    isReady -> {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        )
         setContent {
             Theme(darkTheme = true) {
                 Navigation()
