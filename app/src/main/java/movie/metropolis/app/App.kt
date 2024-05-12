7ab8200708752d4be4ec939957d757ea942427a6
Viktor De Pasquale <hello@depasquale.wiki>
2024-05-12 15:34:33 +0200

    Updated shared preferences to load lazily

--- app/src/main/java/movie/metropolis/app/App.kt
+++ app/src/main/java/movie/metropolis/app/App.kt
@@ -1,19 +1,7 @@
 package movie.metropolis.app
 
 import android.app.Application
-import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        val threadPolicy = StrictMode.ThreadPolicy.Builder()
-            .detectAll()
-            .penaltyLog()
-            .build()
-        StrictMode.setThreadPolicy(threadPolicy)
-    }
-
-}
+class App : Application()
--- style/src/main/java/movie/style/util/Storage.kt
+++ style/src/main/java/movie/style/util/Storage.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.*
+import okio.withLock
+import java.util.concurrent.locks.ReentrantLock
 
 @Composable
 fun rememberSharedPrefs(name: String): SharedPreferences {
     val context = LocalContext.current
-    return remember(context, name) { context.getSharedPreferences(name, Context.MODE_PRIVATE) }
+    val scope = rememberCoroutineScope()
+    return remember(name) {
+        UiSharedPreferences {
+            context.getSharedPreferences(name, Context.MODE_PRIVATE)
+        }
+    }
 }
 
 fun SharedPreferences.getLongArray(key: String, default: LongArray): LongArray {
@@ -17,4 +24,51 @@ fun SharedPreferences.getLongArray(key: String, default: LongArray): LongArray {
 
 fun SharedPreferences.Editor.putLongArray(key: String, value: LongArray) {
     putString(key, value.joinToString(",") { it.toString() })
+}
+
+private class UiSharedPreferences(
+    private val factory: () -> SharedPreferences
+) : SharedPreferences {
+
+    private val lock = ReentrantLock()
+    private var origin: SharedPreferences? = null
+        get() = field ?: lock.withLock {
+            field ?: factory().also {
+                field = it
+            }
+        }
+
+    override fun getAll(): Map<String, *> =
+        origin!!.all
+
+    override fun getString(key: String?, defValue: String?) =
+        origin!!.getString(key, defValue)
+
+    override fun getStringSet(key: String?, defValues: Set<String>?) =
+        origin!!.getStringSet(key, defValues)
+
+    override fun getInt(key: String?, defValue: Int) =
+        origin!!.getInt(key, defValue)
+
+    override fun getLong(key: String?, defValue: Long) =
+        origin!!.getLong(key, defValue)
+
+    override fun getFloat(key: String?, defValue: Float) =
+        origin!!.getFloat(key, defValue)
+
+    override fun getBoolean(key: String?, defValue: Boolean) =
+        origin!!.getBoolean(key, defValue)
+
+    override fun contains(key: String?) =
+        origin!!.contains(key)
+
+    override fun edit() =
+        origin!!.edit()
+
+    override fun registerOnSharedPreferenceChangeListener(listener: SharedPreferences.OnSharedPreferenceChangeListener?) =
+        origin!!.registerOnSharedPreferenceChangeListener(listener)
+
+    override fun unregisterOnSharedPreferenceChangeListener(listener: SharedPreferences.OnSharedPreferenceChangeListener?) =
+        origin!!.unregisterOnSharedPreferenceChangeListener(listener)
+
 }
