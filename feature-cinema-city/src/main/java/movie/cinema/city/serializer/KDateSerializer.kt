acafe3a78dea65dcce3f2af9f333a98f0fd8c4a8
Viktor De Pasquale <hello@depasquale.wiki>
2024-05-12 17:49:40 +0200

    Fixed parsing error while using java classes for whatever reason

--- feature-cinema-city/src/main/java/movie/cinema/city/serializer/KDateSerializer.kt
+++ feature-cinema-city/src/main/java/movie/cinema/city/serializer/KDateSerializer.kt
@@ -8,6 +8,7 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 internal abstract class KDateSerializer : KSerializer<Date> {
 
@@ -17,8 +18,27 @@ internal abstract class KDateSerializer : KSerializer<Date> {
     abstract val type: String
     abstract val formatter: SimpleDateFormat
 
+    private val fallback by lazy {
+        android.icu.text.SimpleDateFormat(
+            formatter.toPattern(),
+            Locale.getDefault()
+        )
+    }
+
     final override fun deserialize(decoder: Decoder): Date {
-        return formatter.parse(decoder.decodeString()).let(::requireNotNull)
+        val str = decoder.decodeString()
+        return try {
+            formatter.parse(str).let(::requireNotNull)
+        } catch (e: Throwable) {
+            try {
+                fallback.parse(str).let(::requireNotNull)
+            } catch (e: Throwable) {
+                throw RuntimeException(
+                    "Parse error: value='$str', format='${formatter.toPattern()}'",
+                    e
+                )
+            }
+        }
     }
 
     final override fun serialize(encoder: Encoder, value: Date) {
