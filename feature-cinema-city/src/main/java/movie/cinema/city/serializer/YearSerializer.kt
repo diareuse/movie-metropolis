8a52df7cf3d7ca777a0b6c747d95e3193682c85f
Viktor De Pasquale <hello@depasquale.wiki>
2024-05-12 17:49:40 +0200

    Fixed parsing error while using java classes for whatever reason

--- feature-cinema-city/src/main/java/movie/cinema/city/serializer/YearSerializer.kt
+++ feature-cinema-city/src/main/java/movie/cinema/city/serializer/YearSerializer.kt
@@ -1,14 +1,29 @@
 package movie.cinema.city.serializer
 
-import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.TimeZone
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.util.Calendar
+import java.util.Date
 
-internal class YearSerializer : KDateSerializer() {
+internal class YearSerializer : KSerializer<Date> {
 
-    override val type: String = "year"
-    override val formatter = SimpleDateFormat("yyyy", Locale.ROOT).apply {
-        timeZone = TimeZone.getTimeZone("UTC")
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("year", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Date {
+        return Calendar.getInstance().apply {
+            time = Date(0)
+            set(Calendar.YEAR, decoder.decodeString().toInt())
+        }.time
     }
 
+    override fun serialize(encoder: Encoder, value: Date) {
+        encoder.encodeString(
+            Calendar.getInstance().apply { time = value }.get(Calendar.YEAR).toString()
+        )
+    }
 }
