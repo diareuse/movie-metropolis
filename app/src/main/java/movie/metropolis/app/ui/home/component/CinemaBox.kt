c464ea7b604a5fa4eb78c863242a25bd68b79723
Viktor De Pasquale <hello@depasquale.wiki>
2025-02-06 17:56:12 +0100

    feat: Introduce background blur to CinemaBox using Haze library
    - Added a background blur to the `CinemaBox` using the Haze library.
    - Implemented `CinemaBoxLayout` to layout the image and content with constraints.
    - Added Haze effect to the content part of the `CinemaBox`.
    - Added Haze as source for the image in `CinemaBox`.
    - Added a background color and noise to the `HazeStyle`
    - Refactored the `CinemaBox` to use `Surface` instead of `Card`.
    - Reordered `name`, `city`, and `distance` into a `Column` with `Row` for `name` and `distance`.

--- app/src/main/java/movie/metropolis/app/ui/home/component/CinemaBox.kt
+++ app/src/main/java/movie/metropolis/app/ui/home/component/CinemaBox.kt
@@ -4,8 +4,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.*
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.HazeTint
+import dev.chrisbanes.haze.hazeEffect
+import dev.chrisbanes.haze.hazeSource
 import movie.metropolis.app.model.CinemaView
 import movie.metropolis.app.screen.cinema.component.CinemaViewProvider
 import movie.style.Image
@@ -21,23 +28,75 @@ fun CinemaBox(
     distance: @Composable () -> Unit,
     image: @Composable () -> Unit,
     modifier: Modifier = Modifier,
-) = Card(
+    haze: HazeState = remember { HazeState() }
+) = Surface(
     modifier = modifier,
     onClick = onClick,
-    shape = MaterialTheme.shapes.large
+    shape = MaterialTheme.shapes.medium
 ) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(100.dp),
-        shape = MaterialTheme.shapes.large
-    ) {
-        image()
-    }
-    Column(modifier = Modifier.padding(2.pc, 1.pc)) {
-        name()
-        city()
-        distance()
+    CinemaBoxLayout(
+        image = {
+            Box(
+                modifier = Modifier.hazeSource(haze),
+                propagateMinConstraints = true
+            ) {
+                image()
+            }
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .hazeEffect(
+                        state = haze,
+                        style = HazeStyle(
+                            backgroundColor = MaterialTheme.colorScheme.surface,
+                            tint = HazeTint(MaterialTheme.colorScheme.surface.copy(.25f)),
+                            noiseFactor = 10f,
+                            blurRadius = 8.dp
+                        )
+                    )
+                    .padding(1.pc, 1.pc),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                ProvideTextStyle(MaterialTheme.typography.labelSmall) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(1.pc)
+                    ) {
+                        name()
+                        distance()
+                    }
+                    Box(modifier = Modifier.alpha(.75f)) {
+                        city()
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
+private fun CinemaBoxLayout(
+    image: @Composable () -> Unit,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    height: Dp = 100.dp
+) = Layout({
+    image()
+    content()
+}, modifier) { (image, content), c ->
+    val imageP = image.measure(
+        Constraints.fixedHeight(height.roundToPx())
+            .copy(minWidth = c.minWidth, maxWidth = c.maxWidth)
+    )
+    val contentP = content.measure(
+        Constraints.fixedWidth(imageP.width)
+            .copy(minHeight = c.minHeight, maxHeight = imageP.height)
+    )
+    val w = imageP.width
+    val h = imageP.height
+    layout(w, h) {
+        imageP.place(0, 0)
+        contentP.place(0, h - contentP.height)
     }
 }
 
