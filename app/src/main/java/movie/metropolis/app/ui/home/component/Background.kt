85408a3285080ada849ac818dd1f91850b3e8ca3
Viktor De Pasquale <hello@depasquale.wiki>
2025-02-16 18:29:45 +0100

    feat: Improve Background gradient
    - Updated the background gradient to use three radial gradients for a richer effect.
    - Replaced the solid background color with gradients.
    - Added a red radial gradient for more depth.
    - Added preview with `Surface` for better visibility.

--- app/src/main/java/movie/metropolis/app/ui/home/component/Background.kt
+++ app/src/main/java/movie/metropolis/app/ui/home/component/Background.kt
@@ -1,6 +1,7 @@
 package movie.metropolis.app.ui.home.component
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.draw.*
@@ -10,38 +11,45 @@ import androidx.compose.ui.tooling.preview.*
 import movie.style.layout.PreviewLayout
 import kotlin.math.hypot
 
-fun Modifier.windowBackground(background: Color = Color.Black) = drawWithCache {
+fun Modifier.windowBackground() = drawWithCache {
+    val red = Color(0xFFFF0800)
+    val pink = Color(0xFFF01FFF)
+    val purple = Color(0xFF7700FF)
+    val transparent = Color.Transparent
+    val b1 = Brush.radialGradient(
+        colors = listOf(
+            pink.copy(.125f),
+            purple.copy(.025f)
+        ),
+        radius = hypot(size.height * .34f, size.width),
+        center = Offset.Zero
+    )
+    val b2 = Brush.radialGradient(
+        colors = listOf(red.copy(.05f), transparent),
+        center = Offset(size.width, size.height / 2),
+        radius = size.maxDimension / 3
+    )
+    val b3 = Brush.radialGradient(
+        colors = listOf(purple.copy(.1f), transparent),
+        center = Offset(size.width, size.height),
+        radius = size.maxDimension / 2
+    )
     onDrawWithContent {
-        Color(0xFFFF0800)
-        val pink = Color(0xFFF01FFF)
-        val purple = Color(0xFF7700FF)
-        Color.White
-        val transparent = Color.Transparent
-        val b1 = Brush.radialGradient(
-            listOf(
-                pink.copy(.125f),
-                purple.copy(.025f)
-            ), radius = hypot(size.height * .34f, size.width), center = Offset.Zero
-        )
-        val b3 = Brush.radialGradient(
-            listOf(purple.copy(.1f), transparent),
-            center = Offset(size.width, size.height),
-            radius = size.maxDimension / 2
-        )
-
         drawRect(b1)
-        //drawRect(b2)
+        drawRect(b2)
         drawRect(b3)
         drawContent()
     }
 }
 
-@Preview
+@PreviewLightDark
 @Composable
 private fun BackgroundPreview() = PreviewLayout {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .windowBackground()
-    )
+    Surface {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .windowBackground()
+        )
+    }
 }
